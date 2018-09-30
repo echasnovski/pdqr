98705f8fd2bb07f308a6ec4aab3ec8cb7e8f672c
echasnovski <evgeni.chasnovski@gmail.com>
2018-09-30 12:44:08 +0300

    Implement `assert_type()`.

---
+++ R/assertions.R
@@ -0,0 +1,53 @@
+# Assert object type ------------------------------------------------------
+#' Assert object type
+#'
+#' Throw an error in case object is not of desired type.
+#'
+#' @param x An object to check.
+#' @param predicate A function to perform check. A good idea is to use function
+#'   named `is.*()` or `is_*()` with possible `<package>::` prefix.
+#' @param type A string for desired type. If `NULL`, type is taken from parsing
+#'   original name of supplied `predicate`: all alphanumeric with '_' and '.'
+#'   characters (until the name end) after the first appearance of either `is.`
+#'   or `is_`. In case of a doubt supply `type` explicitly.
+#'
+#' @examples
+#' \dontrun{
+#' x <- 1
+#' assert_type(x, is.numeric)
+#' assert_type(x, is.logical)
+#' assert_type(x, rlang::is_string, "character of length 1")
+#' }
+#'
+#' @keywords internal
+#' @noRd
+assert_type <- function(x, predicate, type = NULL) {
+  x_name <- deparse(rlang::enexpr(x))
+  if (is.null(type)) {
+    predicate_name <- deparse(rlang::enexpr(predicate))
+    type <- parse_type(predicate_name)
+  }
+
+  if (!isTRUE(predicate(x))) {
+    # Not using "must be of type" because of 'tibble' and 'string' cases
+    stop_glue("`{x_name}` must be '{type}', not '{get_type(x)}'.")
+  }
+
+  x
+}
+
+# This function is needed because `typeof()` on data frame returns "list"
+get_type <- function(x) {
+  if (is.data.frame(x)) {
+    return("data.frame")
+  }
+
+  typeof(x)
+}
+
+parse_type <- function(f_name) {
+  regmatches(
+    f_name,
+    regexec("is[_\\.]([[:alnum:]_\\.]+)$", f_name)
+  )[[1]][2]
+}
---
+++ tests/testthat/test-assertions.R
@@ -0,0 +1,37 @@
+context("test-assertions")
+
+
+# assert_type -------------------------------------------------------------
+test_that("assert_type works", {
+  x_var <- 1L
+
+  expect_equal(assert_type(x_var, is.integer), x_var)
+  expect_silent(assert_type(x_var, is.integer))
+
+  expect_error(assert_type(x_var, is.character), "x_var.*character.*integer")
+  expect_error(
+    assert_type(x_var, is.character, "symbolic"), "x_var.*symbolic.*integer"
+  )
+
+  x_df <- data.frame(x = TRUE)
+  expect_silent(assert_type(x_df, is.data.frame))
+  expect_error(
+    assert_type(x_df, is.logical), "x_df.*logical.*data\\.frame"
+  )
+})
+
+
+# get_type ----------------------------------------------------------------
+test_that("get_type works", {
+  expect_equal(get_type(1L), "integer")
+  expect_equal(get_type(1), "double")
+  expect_equal(get_type(data.frame(x = 1)), "data.frame")
+})
+
+
+# parse_type --------------------------------------------------------------
+test_that("parse_type works", {
+  expect_equal(parse_type("is.numeric"), "numeric")
+  expect_equal(parse_type("is_number"), "number")
+  expect_equal(parse_type("is_is1_logical"), "is1_logical")
+})
