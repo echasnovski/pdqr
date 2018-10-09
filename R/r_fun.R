f4b0935c381511d4496a8c5823937f926abe5ecc
echasnovski <evgeni.chasnovski@gmail.com>
2018-10-09 21:02:10 +0300

    Implement `r_fun()`.

---
+++ R/r_fun.R
@@ -0,0 +1,46 @@
+r_fun <- function(x, type = "smooth", attach_sample = FALSE, extra = NULL,
+                  ...) {
+  distr_impl(
+    fun_class = "r_fun",
+    impl_funs = list(raw = r_fun_raw, smooth = r_fun_smooth),
+    x = x, type = type, attach_sample = attach_sample, extra = extra, ...
+  )
+}
+
+r_fun_raw <- function(x) {
+  q_raw <- q_fun(x, type = "raw", attach_sample = FALSE)
+
+  # For efficient memory management
+  rm(list = "x", envir = environment())
+
+  res <- function(n) {
+    rand_q_vec <- stats::runif(n, min = 0, max = 1)
+
+    q_raw(rand_q_vec)
+  }
+
+  add_meta(
+    res,
+    distr_tbl = meta(q_raw, "distr_tbl"),
+    domain_out = meta(q_raw, "domain_out")
+  )
+}
+
+r_fun_smooth <- function(x, ...) {
+  q_smooth <- q_fun(x, type = "smooth", attach_sample = FALSE, ...)
+
+  # For efficient memory management
+  rm(list = "x", envir = environment())
+
+  res <- function(n) {
+    rand_q_vec <- stats::runif(n, min = 0, max = 1)
+
+    q_smooth(rand_q_vec)
+  }
+
+  add_meta(res, domain_out = meta(q_smooth, "domain_out"))
+}
+
+print.r_fun <- function(x, ...) {
+  distr_print("Random generation function", x, ...)
+}
---
+++ tests/testthat/test-r_fun.R
@@ -0,0 +1,82 @@
+context("test-r_fun")
+
+# Input data --------------------------------------------------------------
+r_raw <- r_fun(x_raw, type = "raw")
+r_smooth <- r_fun(x_smooth, type = "smooth")
+
+
+# r_fun -------------------------------------------------------------------
+test_that("r_fun works", {
+  expect_distr_fun(r_raw, "r_fun", "raw")
+  expect_equal(meta(r_raw, "domain_out"), x_raw_domain_in)
+  expect_true(all(r_raw(100) %in% x_raw_distr_tbl[["x"]]))
+
+  expect_distr_fun(r_smooth, "r_fun", "smooth")
+  expect_equal(
+    round(meta(r_smooth, "domain_out"), 2), round(x_smooth_domain_in, 2)
+  )
+  r_smooth_out <- r_smooth(100)
+  expect_true(all(
+    (r_smooth_out >= x_smooth_domain_in[1]) &
+      (r_smooth_out <= x_smooth_domain_in[2])
+  ))
+})
+
+test_that("r_fun asserts", {
+  expect_error(r_fun("a"), "x.*numeric")
+  expect_error(r_fun(x_raw, type = 1), "type.*string")
+  expect_error(r_fun(x_raw, type = "a"), "type.*raw.*smooth")
+  expect_error(r_fun(x_raw, attach_sample = NULL), "attach_sample.*TRUE.*FALSE")
+})
+
+test_that("r_fun handles meta data", {
+  expect_equal(
+    meta(r_raw),
+    list(
+      distr_tbl = x_raw_distr_tbl, domain_out = x_raw_domain_in, type = "raw"
+    )
+  )
+
+  r_smooth_1 <- r_fun(x_smooth, type = "smooth", attach_sample = TRUE)
+  expect_named(meta(r_smooth_1), c("domain_out", "sample", "type"))
+  expect_equal(
+    round(meta(r_smooth_1, "domain_out"), 2), round(x_smooth_domain_in, 2)
+  )
+  expect_equal(
+    meta(r_smooth_1)[c("sample", "type")],
+    list(sample = x_smooth, type = "smooth")
+  )
+
+  r_smooth_2 <- r_fun(x_smooth, type = "smooth", extra = list(a = TRUE))
+  expect_named(meta(r_smooth_2), c("domain_out", "extra", "type"))
+  expect_equal(meta(r_smooth_2, "extra"), list(a = TRUE))
+})
+
+
+# r_fun_raw ---------------------------------------------------------------
+# Tested in `r_fun()`
+
+
+# r_fun_smooth ------------------------------------------------------------
+# Tested in `r_fun()`
+
+
+# print.r_fun -------------------------------------------------------------
+test_that("print.r_fun works", {
+  expect_output(
+    print(r_raw),
+    paste0(
+      "Random generation function.*raw.*",
+      "[mM]eta.*distr_tbl, domain_out, type.*function"
+    )
+  )
+
+  r_smooth_extra <- r_fun(x_smooth, type = "smooth", extra = "a")
+  expect_output(
+    print(r_smooth_extra),
+    paste0(
+      "Random generation function.*smoothed.*",
+      "[mM]eta.*domain_out, extra, type.*function"
+    )
+  )
+})
