53ec1c52a1a1a8f6854f9b19ffd81d3281dc965f
echasnovski <evgeni.chasnovski@gmail.com>
2018-10-28 14:49:55 +0200

    Implement `plot()` and `lines()` methods for "p_fun", "d_fun", "q_fun", "r_fun".

---
+++ R/plot.R
@@ -0,0 +1,110 @@
+# plot() ------------------------------------------------------------------
+plot.p_fun <- function(x, y = NULL, n_grid = 1001, ...) {
+  x_name <- deparse(substitute(x))
+  dots <- make_plot_dots(
+    ...,
+    type = "l", main = paste0("Cumulative distribution function ", x_name),
+    xlab = "x", ylab = "Cumulative probability"
+  )
+
+  # Stretch domain to guarantee 0 and 1 on edges
+  plot_impl_pdq(x, meta(x, "domain_in") + 10^(-6) * c(-1, 1), n_grid, dots)
+}
+
+plot.d_fun <- function(x, y = NULL, n_grid = 1001, ...) {
+  x_name <- deparse(substitute(x))
+
+  warn_plotting_raw_d_fun(x)
+
+  dots <- make_plot_dots(
+    ...,
+    type = "l", main = paste0("Density function ", x_name),
+    xlab = "x", ylab = "Density"
+  )
+
+  plot_impl_pdq(x, meta(x, "domain_in"), n_grid, dots)
+}
+
+plot.q_fun <- function(x, y = NULL, n_grid = 1001, ...) {
+  x_name <- deparse(substitute(x))
+  dots <- make_plot_dots(
+    ...,
+    type = "l", main = paste0("Quantile function ", x_name),
+    xlab = "Cumulative probability", ylab = "x"
+  )
+
+  plot_impl_pdq(x, c(0, 1), n_grid, dots)
+}
+
+plot.r_fun <- function(x, y = NULL, n_sample = 1001, ...) {
+  x_name <- deparse(substitute(x))
+
+  smpl <- x(n_sample)
+  hist_args <- make_plot_dots(
+    x = smpl,
+    ...,
+    main = paste0("Output of random generation function ", x_name),
+    xlab = "x"
+  )
+
+  do.call(graphics::hist, hist_args)
+
+  invisible()
+}
+
+plot_impl_pdq <- function(f, grid_range, n_grid, dots) {
+  grid <- seq(grid_range[1], grid_range[2], length.out = n_grid)
+  plot_args <- dedupl_list(c(list(x = grid, y = f(grid)), dots))
+
+  do.call(graphics::plot, plot_args)
+}
+
+make_plot_dots <- function(...) {
+  dedupl_list(list(...))
+}
+
+dedupl_list <- function(l) {
+  l_names <- names(l)
+
+  if (is.null(l_names)) {
+    l
+  } else {
+    l[!duplicated(l_names) | (l_names == "")]
+  }
+}
+
+warn_plotting_raw_d_fun <- function(f) {
+  if ((class(f)[1] == "d_fun") && (meta(f, "type") == "raw")) {
+    warning_collapse(
+      'Plotting density function with `type` = "raw" can miss actual points.'
+    )
+  }
+
+  f
+}
+
+
+# lines() -----------------------------------------------------------------
+lines.p_fun <- function(x, n_grid = 1001, ...) {
+  # Stretch domain to guarantee 0 and 1 on edges
+  lines_impl_pdq(
+    x, meta(x, "domain_in") + 10^(-6) * c(-1, 1), n_grid, list(...)
+  )
+}
+
+lines.d_fun <- function(x, n_grid = 1001, ...) {
+  warn_plotting_raw_d_fun(x)
+
+  lines_impl_pdq(x, meta(x, "domain_in"), n_grid, list(...))
+}
+
+lines.q_fun <- function(x, n_grid = 1001, ...) {
+  lines_impl_pdq(x, c(0, 1), n_grid, list(...))
+}
+
+lines_impl_pdq <- function(f, grid_range, n_grid, dots) {
+  grid <- seq(grid_range[1], grid_range[2], length.out = n_grid)
+  lines_args <- dedupl_list(c(list(x = grid, y = f(grid)), dots))
+
+  do.call(graphics::lines, lines_args)
+}
---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,265 @@
+context("test-plot")
+
+set.seed(5555)
+
+# All tested plot calls are wrapped in anonymus functions to avoid printing of
+# the result of their calls (`NULL`). This seems to be a result of {vdiffr}
+# approach: it `print()`s its input (resulting here with `NULL`), unless wrapped
+# in function.
+
+# plot.p_fun --------------------------------------------------------------
+test_that("plot.p_fun works", {
+  vdiffr::expect_doppelganger("plot-p-fun-1", function() {plot(p_raw_withx)})
+  vdiffr::expect_doppelganger(
+    "plot-p-fun-2", function() {plot(p_raw_withx, y = NA)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-p-fun-3", function() {
+      plot(p_raw_withx, xlab = "a", ylab = "b", main = "c", type = "p")
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-p-fun-4", function() {
+      plot(p_raw_withx, xlim = c(0, 2), col = "blue")
+    }
+  )
+
+  # Thise are also tests for `lines.p_fun()`
+  vdiffr::expect_doppelganger(
+    "plot-lines-p-fun-1", function() {
+      plot(p_smooth_withx)
+      lines(p_custom)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-p-fun-2", function() {
+      plot(p_smooth_withx, y = NA)
+      lines(p_custom, y = NA)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-p-fun-3", function() {
+      plot(p_smooth_withx, xlab = "a", ylab = "b", main = "c", type = "p")
+      lines(p_custom, lwd = 2)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-p-fun-4", function() {
+      plot(p_smooth_withx, xlim = c(0, 2), col = "blue")
+      lines(p_custom)
+    }
+  )
+})
+
+test_that("plot.p_fun and lines.p_fun respect `n_grid` argument", {
+  vdiffr::expect_doppelganger(
+    "plot-lines-p-fun-n-grid", function() {
+      plot(p_smooth_withx, n_grid = 6)
+      lines(p_custom, n_grid = 6)
+    }
+  )
+})
+
+
+# plot.d_fun --------------------------------------------------------------
+test_that("plot.d_fun works", {
+  density_warning <- "[Dd]ensity.*raw.*can miss"
+
+  vdiffr::expect_doppelganger(
+    "plot-d-fun-1", function() {
+      expect_warning(plot(d_raw_withx), density_warning)
+      expect_warning(lines(d_raw_withx, col = "red"), density_warning)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-d-fun-2", function() {
+      expect_warning(plot(d_raw_withx, y = NA), density_warning)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-d-fun-3", function() {
+      expect_warning(
+        plot(d_raw_withx, xlab = "a", ylab = "b", main = "c", type = "p"),
+        density_warning
+      )
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-d-fun-4", function() {
+      expect_warning(
+        plot(d_raw_withx, xlim = c(0, 2), col = "blue"),
+        density_warning
+      )
+    }
+  )
+
+  # Thise are also tests for `lines.d_fun()`
+  vdiffr::expect_doppelganger(
+    "plot-lines-d-fun-1", function() {
+      plot(d_smooth_withx)
+      lines(d_custom)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-d-fun-2", function() {
+      plot(d_smooth_withx, y = NA)
+      lines(d_custom, y = NA)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-d-fun-3", function() {
+      plot(d_smooth_withx, xlab = "a", ylab = "b", main = "c", type = "p")
+      lines(d_custom, lwd = 2)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-d-fun-4", function() {
+      plot(d_smooth_withx, xlim = c(0, 2), col = "blue")
+      lines(d_custom)
+    }
+  )
+})
+
+test_that("plot.d_fun and lines.d_fun respect `n_grid` argument", {
+  vdiffr::expect_doppelganger(
+    "plot-lines-d-fun-n-grid", function() {
+      plot(d_smooth_withx, n_grid = 6)
+      lines(d_custom, n_grid = 6)
+    }
+  )
+})
+
+
+# plot.q_fun --------------------------------------------------------------
+test_that("plot.q_fun works", {
+  vdiffr::expect_doppelganger("plot-q-fun-1", function() {plot(q_raw_withx)})
+  vdiffr::expect_doppelganger(
+    "plot-q-fun-2", function() {plot(q_raw_withx, y = NA)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-q-fun-3", function() {
+      plot(q_raw_withx, xlab = "a", ylab = "b", main = "c", type = "p")
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-q-fun-4", function() {
+      plot(q_raw_withx, xlim = c(0, 2), col = "blue")
+    }
+  )
+
+  # Thise are also tests for `lines.q_fun()`
+  vdiffr::expect_doppelganger(
+    "plot-lines-q-fun-1", function() {
+      plot(q_smooth_withx)
+      lines(q_custom)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-q-fun-2", function() {
+      plot(q_smooth_withx, y = NA)
+      lines(q_custom, y = NA)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-q-fun-3", function() {
+      plot(q_smooth_withx, xlab = "a", ylab = "b", main = "c", type = "p")
+      lines(q_custom, lwd = 2)
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-lines-q-fun-4", function() {
+      plot(q_smooth_withx, xlim = c(0, 2), col = "blue")
+      lines(q_custom)
+    }
+  )
+})
+
+test_that("plot.q_fun and lines.q_fun respect `n_grid` argument", {
+  vdiffr::expect_doppelganger(
+    "plot-lines-q-fun-n-grid", function() {
+      plot(q_smooth_withx, n_grid = 6)
+      lines(q_custom, n_grid = 6)
+    }
+  )
+})
+
+
+# plot.r_fun --------------------------------------------------------------
+test_that("plot.r_fun works", {
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-raw-1", function() {plot(r_raw_withx)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-raw-2", function() {plot(r_raw_withx, y = NA)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-raw-3", function() {
+      plot(r_raw_withx, xlab = "a", main = "c")
+    }
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-raw-4", function() {
+      plot(r_raw_withx, xlim = c(0, 2), col = "blue", freq = FALSE)
+    }
+  )
+
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-smooth-1", function() {plot(r_smooth_withx)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-smooth-2", function() {plot(r_smooth_withx, y = NA)}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-smooth-3",
+    function() {plot(r_smooth_withx, xlab = "a", main = "c")}
+  )
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-smooth-4",
+    function() {
+      plot(r_smooth_withx, xlim = c(0, 2), col = "blue", freq = FALSE)
+    }
+  )
+})
+
+test_that("plot.r_fun respects `n_sample` argument", {
+  vdiffr::expect_doppelganger(
+    "plot-r-fun-n-sample", function() {plot(r_smooth_withx, n_sample = 10)}
+  )
+})
+
+
+# plot_impl_pdq -----------------------------------------------------------
+# Tested in `plot()` methods
+
+
+# make_plot_dots ----------------------------------------------------------
+# Tested in `plot()` methods
+
+
+# dedupl_list -------------------------------------------------------------
+test_that("dedupl_list works", {
+  input_1 <- list(1, 2)
+  expect_equal(dedupl_list(input_1), input_1)
+
+  input_2 <- list(1, 2, c = 3)
+  expect_equal(dedupl_list(input_2), input_2)
+
+  input_3 <- list(a = 1, 2, c = 3, a = 4)
+  expect_equal(dedupl_list(input_3), input_3[-4])
+})
+
+
+# lines.p_fun -------------------------------------------------------------
+# Tested in `plot()` methods
+
+
+# lines.d_fun -------------------------------------------------------------
+# Tested in `plot()` methods
+
+
+# lines.q_fun -------------------------------------------------------------
+# Tested in `plot()` methods
+
+
+# lines_impl_pdq ----------------------------------------------------------
+# Tested in `plot()` methods
