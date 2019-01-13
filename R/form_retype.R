4210d3d99a9a37cce64440765f9c548d9db730ba
echasnovski <evgeni.chasnovski@gmail.com>
2019-01-13 21:17:30 +0200

    Implement `form_retype()`.

---
+++ R/form_retype.R
@@ -0,0 +1,106 @@
+form_retype <- function(f, type) {
+  assert_pdqr_fun(f)
+  assert_distr_type(type)
+
+  switch(
+    type,
+    fin = retype_fin(f),
+    infin = retype_infin(f)
+  )
+}
+
+retype_fin <- function(f) {
+  if (meta(f, "type") == "fin") {
+    return(f)
+  }
+
+  x_tbl <- meta(f, "x_tbl")
+  n <- nrow(x_tbl)
+
+  x_lag <- x_tbl[["x"]][-n]
+  x_lead <- x_tbl[["x"]][-1]
+  y_lag <- x_tbl[["y"]][-n]
+  y_lead <- x_tbl[["y"]][-1]
+  y_sum <- y_lag + y_lead
+
+  # Output `x` values are computed as intervals' centres of mass
+  x_mass <- (x_lag * (y_lag + y_sum) + x_lead * (y_lead + y_sum)) / (3 * y_sum)
+    # If interval has zero probability then its centre is set to the middle
+  x_mass_bad <- !is.finite(x_mass)
+  x_mass[x_mass_bad] <- (x_lag[x_mass_bad] + x_lead[x_mass_bad]) / 2
+
+  # Output probabilities are computed as probabilites (mass) of intervals
+  prob <- diff(x_tbl[["cumprob"]])
+
+  # Creating pdqr-function
+  pdqr_fun <- impute_pdqr_fun(pdqr_class = NULL, ref = f)
+
+  pdqr_fun(data.frame(x = x_mass, prob = prob), "fin")
+}
+
+retype_infin <- function(f) {
+  if (meta(f, "type") == "infin") {
+    return(f)
+  }
+
+  # Collapse duplicate `x` values
+  x_tbl <- normalize_fin_x_tbl(meta(f, "x_tbl"))
+
+  n <- nrow(x_tbl)
+  if (n < 4) {
+    stop_collapse(
+      'For conversion to "infin" type `form_retype()` needs at least 4 unique ',
+      '`x` values.'
+    )
+  }
+
+  x <- x_tbl[["x"]]
+  prob <- x_tbl[["prob"]]
+
+  # Values of `x` grid (except first and last elements) of "infin" output is
+  # approximated as convex combination of nearest "centres of mass":
+  # `x_mid = (1-alpha)*x_mass_left + alpha*x_mass_right`. Here `alpha` is
+  # approximated based on two separate assumptions:
+  # *Assumption 1*: if locally `y_left` (`y` value in "true" `x` to the left of
+  # `x_mid`) = `y_mid` = `y_right` (by analogy with `y_left`), then
+  # `alpha = prob_left / (prob_left + prob_right)`.
+  # *Assumption 2*: if locally "true" values of `x` are equidistant then `alpha`
+  # lie inside [1/3; 2/3] interval.
+  # Final approximation is formed by combining these conclusions
+  prob_sum <- prob[1:(n-1)] + prob[2:n]
+  alpha <- pmin(pmax(prob[1:(n-1)] / prob_sum, 1/3), 2/3)
+  alpha[!is.finite(alpha)] <- 0.5
+
+  x_grid <- numeric(n + 1)
+  x_grid[2:n] <- (1 - alpha) * x[1:(n-1)] + alpha * x[2:n]
+  # First and last `x` are approximated so that first and last `x` triplets are
+  # equidistant
+  x_grid[1] <- x_grid[2] - (x_grid[3] - x_grid[2])
+  x_grid[n+1] <- x_grid[n] + (x_grid[n] - x_grid[n-1])
+
+  # Output `y` grid is approximated in 'pdqr' fashion
+  p_grid <- c(0, cumsum(prob))
+  y <- y_from_p_grid(x_grid, p_grid)
+
+  # Creating pdqr-function
+  pdqr_fun <- impute_pdqr_fun(pdqr_class = NULL, ref = f)
+
+  pdqr_fun(data.frame(x = x_grid, y = y), "infin")
+}
+
+normalize_fin_x_tbl <- function(x_tbl) {
+  x <- x_tbl[["x"]]
+  vals <- sort(unique(x))
+
+  if (!is.unsorted(x) && (length(x) == length(vals))) {
+    return(x_tbl)
+  }
+
+  x_val_id <- match(x, vals)
+
+  # `as.vector()` is used to remove extra attributes
+  prob_new <- as.vector(tapply(x_tbl[["prob"]], x_val_id, sum))
+  prob_new <- prob_new / sum(prob_new)
+
+  data.frame(x = vals, prob = prob_new, cumprob = cumsum(prob_new))
+}
---
+++ tests/testthat/test-form_retype.R
@@ -0,0 +1,112 @@
+context("test-form_retype")
+
+set.seed(12321)
+
+
+# form_retype -------------------------------------------------------------
+test_that("form_retype converts to `type` 'fin'", {
+  output <- form_retype(p_infin, "fin")
+  expect_distr_fun(output, "p", "fin")
+
+  # "infin" function should have one row more in "x_tbl" than "fin" function
+  expect_equal(nrow(meta(p_infin, "x_tbl")) - 1, nrow(meta(output, "x_tbl")))
+})
+
+test_that("form_retype converts to `type` 'infin'", {
+  output <- form_retype(d_fin, "infin")
+  expect_distr_fun(output, "d", "infin")
+
+  # "infin" function should have one row more in "x_tbl" than "fin" function
+  expect_equal(nrow(meta(d_fin, "x_tbl")), nrow(meta(output, "x_tbl")) - 1)
+})
+
+test_that("form_retype correctly handles adjacent zero densities", {
+  input_infin <- new_d(data.frame(x = 1:4, y = c(1, 0, 0, 1)), "infin")
+  output_fin <- form_retype(input_infin, "fin")
+
+  # "Centre of mass" in case both ends have zero density should be in the middle
+  expect_equal(meta(output_fin, "x_tbl")[["x"]][2], 2.5)
+})
+
+test_that("form_retype converts accurately 'infin'->'fin'->'infin'", {
+  skip_on_cran()
+
+  error <- vapply(
+    fam_list[c("fam_norm", "fam_exp", "fam_exp_rev", "fam_beta", "fam_chisq")],
+    function(fam) {
+      d_f <- as_d(fam$d, fam$support)
+      d_f_new <- form_retype(form_retype(d_f, "fin"), "infin")
+
+      # `quan999()` is used instead of `max()` because retyping can slightly
+      # shrink "true" support (and signigicantly, if support is set manually).
+      # It means that outside of output's support error can be quite high.
+      quan999(abs(d_f(fam$grid) - d_f_new(fam$grid)))
+    },
+    numeric(1))
+  expect_true(all(error < 1e-6))
+})
+
+test_that("form_retype converts accurately 'fin'->'infin'->'fin'", {
+  skip_on_cran()
+
+  test_grid <- seq(-4, 4, length.out = 1e5)
+  error <- vapply(
+    1:10,
+    function(i) {
+      n <- sample(1:1000, 1)
+      x <- sort(rnorm(n))
+      prob <- runif(n)
+      prob <- prob / sum(prob)
+
+      p_f <- new_p(data.frame(x = x, prob = prob), "fin")
+      p_f_new <- form_retype(form_retype(p_f, "infin"), "fin")
+
+      max(abs(p_f(test_grid) - p_f_new(test_grid)))
+    },
+    numeric(1))
+
+  # Accuracy is quite low because of possibility of very close `x` values in
+  # input with almost the same probability as in other. It induces very high
+  # values of "infin" density which can disturb all CDF.
+  expect_true(all(error < 0.1))
+})
+
+test_that("form_retype returns input when types match", {
+  expect_identical(form_retype(d_fin, "fin"), d_fin)
+  expect_identical(form_retype(d_infin, "infin"), d_infin)
+})
+
+test_that("form_retype asserts bad input", {
+  expect_error(form_retype("a", "fin"), "`f`.*function")
+  expect_error(form_retype(d_fin, "a"), '`type`.*"fin".*"infin"')
+
+  fin_small_n_vals <- new_d(1:3, "fin")
+  expect_error(form_retype(fin_small_n_vals, "infin"), "4.*values")
+})
+
+
+# retype_fin --------------------------------------------------------------
+# Tested in `form_retype()`
+
+
+# retype_infin ------------------------------------------------------------
+# Tested in `form_retype()`
+
+
+# normalize_fin_x_tbl -----------------------------------------------------
+test_that("normalize_fin_x_tbl works", {
+  good_input <- meta(d_fin, "x_tbl")
+  expect_identical(normalize_fin_x_tbl(good_input), good_input)
+
+  input_1 <- data.frame(x = c(1, 1, 2), prob = c(0.25, 0.25, 0.5))
+  expect_equal(
+    normalize_fin_x_tbl(input_1),
+    data.frame(x = c(1, 2), prob = c(0.5, 0.5), cumprob = c(0.5, 1))
+  )
+
+  input_2 <- data.frame(x = c(1, -2, 1, -2), prob = c(0, 0.5, 0, 0.5))
+  expect_equal(
+    normalize_fin_x_tbl(input_2),
+    data.frame(x = c(-2, 1), prob = c(1, 0), cumprob = c(1, 1))
+  )
+})
