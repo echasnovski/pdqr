e14462ab416bb7469cf51e7bb810b21d1189b8cb
echasnovski <evgeni.chasnovski@gmail.com>
2019-02-12 21:41:16 +0200

    Implement `form_tails()`.

---
+++ R/form_tails.R
@@ -0,0 +1,144 @@
+form_tails <- function(f, level, method = "trim", direction = "both") {
+  assert_form_tails_args(f, level, method, direction)
+
+  switch(
+    method,
+    trim = tails_trim(f, level, direction),
+    winsor = tails_winsor(f, level, direction)
+  )
+}
+
+tails_trim <- function(f, level, direction = "both") {
+  is_all_trimmed <- ((direction %in% c("left", "right")) && (level == 1)) ||
+    ((direction == "both") && (level == 0.5))
+  if (is_all_trimmed) {
+    return(trim_all(f, direction))
+  }
+
+  switch(
+    meta_type(f),
+    fin = tails_trim_fin(f, level, direction),
+    infin = tails_trim_infin(f, level, direction)
+  )
+}
+
+tails_winsor <- function(f, level, direction = "both") {
+  new_supp <- compute_support_after_remove(f, level, direction)
+
+  form_resupport(f, new_supp, method = "winsor")
+}
+
+tails_trim_fin <- function(f, level, direction) {
+  f_x_tbl <- meta_x_tbl(f)
+
+  new_supp <- compute_support_after_remove(f, level, direction)
+
+  if (direction %in% c("left", "both")) {
+    x_is_to_remove <- f_x_tbl[["x"]] < new_supp[1]
+    tot_p_to_remove <- sum(f_x_tbl[["prob"]][x_is_to_remove])
+    f_x_tbl <- f_x_tbl[!x_is_to_remove, ]
+
+    # Delete "underremoved" probability from left "x" value. If all probability
+    # is removed, the whole row is removed.
+    f_x_tbl <- decrease_row_prob(
+      f_x_tbl, row = 1, by_prob = level - tot_p_to_remove
+    )
+  }
+
+  if (direction %in% c("right", "both")) {
+    x_is_to_remove <- f_x_tbl[["x"]] > new_supp[2]
+    tot_p_to_remove <- sum(f_x_tbl[["prob"]][x_is_to_remove])
+    f_x_tbl <- f_x_tbl[!x_is_to_remove, ]
+
+    # Delete "underremoved" probability from right "x" value. If all probability
+    # is removed, the whole row is removed.
+    f_x_tbl <- decrease_row_prob(
+      f_x_tbl, row = nrow(f_x_tbl), by_prob = level - tot_p_to_remove
+    )
+  }
+
+  new_pdqr_by_ref(f)(f_x_tbl, "fin")
+}
+
+tails_trim_infin <- function(f, level, direction) {
+  new_supp <- compute_support_after_remove(f, level, direction)
+
+  form_resupport(f, new_supp, method = "trim")
+}
+
+assert_form_tails_args <- function(f, level, method, direction) {
+  assert_pdqr_fun(f)
+  assert_type(level, is_single_number, "single number")
+  if (level < 0) {
+    stop_collapse("`level` should not be negative.")
+  }
+
+  assert_type(method, is_string)
+  if (!(method %in% c("trim", "winsor"))) {
+    stop_collapse('`method` should be one of "trim", "winsor".')
+  }
+
+  assert_type(direction, is_string)
+  if (!(direction %in% c("left", "right", "both"))) {
+    stop_collapse('`direction` should be one of "left", "right", "both".')
+  }
+
+  if (direction == "both") {
+    if (level > 0.5) {
+      stop_collapse(
+        '`level` should not be greater than 0.5 in case `direction` is "both".'
+      )
+    }
+  } else {
+    if (level > 1) {
+      stop_collapse(
+        '`level` should not be greater than 1 in case `direction` is one of ',
+        '"left" or "right".'
+      )
+    }
+  }
+
+  TRUE
+}
+
+trim_all <- function(f, direction) {
+  f_x_tbl <- meta_x_tbl(f)
+
+  res_x <- switch(
+    direction,
+    left = max(f_x_tbl[["x"]]),
+    right = min(f_x_tbl[["x"]]),
+    both = as_q.pdqr(f)(0.5)
+  )
+
+  point_dirac(at_x = res_x, meta_type(f), get_pdqr_class(f))
+}
+
+compute_support_after_remove <- function(f, level, direction) {
+  supp <- meta_support(f)
+  q_f <- as_q.pdqr(f)
+
+  if (direction %in% c("left", "both")) {
+    left_val <- q_f(level)
+  } else {
+    left_val <- supp[1]
+  }
+  if (direction %in% c("right", "both")) {
+    right_val <- q_f(1 - level)
+  } else {
+    right_val <- supp[2]
+  }
+
+  c(left_val, right_val)
+}
+
+decrease_row_prob <- function(x_tbl, row, by_prob) {
+  res_row_prob <- x_tbl[["prob"]][row] - by_prob
+  if (res_row_prob == 0) {
+    x_tbl <- x_tbl[-row, ]
+  } else {
+    x_tbl[["prob"]][row] <- res_row_prob
+  }
+
+  x_tbl
+}
---
+++ tests/testthat/test-form_tails.R
@@ -0,0 +1,255 @@
+context("test-form_tails")
+
+
+# Custom expectations -----------------------------------------------------
+expect_self_x_tbl <- function(pdqr_f) {
+  for (method in c("trim", "winsor")) {
+    for (dir in c("both", "left", "right")) {
+      expect_equal(
+        meta_x_tbl(form_tails(pdqr_f, 0, method, dir)),
+        meta_x_tbl(pdqr_f)
+      )
+    }
+  }
+}
+
+expect_dirac <- function(pdqr_f, x_vec) {
+  max_levels <- c("both" = 0.5, "left" = 1, "right" = 1)
+
+  for (method in c("trim", "winsor")) {
+    for (dir in c("both", "left", "right")) {
+      expect_equal(
+        meta_x_tbl(form_tails(pdqr_f, max_levels[dir], method, dir)),
+        meta_x_tbl(
+          point_dirac(x_vec[dir], meta_type(pdqr_f), get_pdqr_class(pdqr_f))
+        )
+      )
+    }
+  }
+}
+
+expect_error_negative_level <- function(pdqr_f) {
+  for (method in c("trim", "winsor")) {
+    for (dir in c("both", "left", "right")) {
+      expect_error(
+        form_tails(pdqr_f, -0.1, method = method, direction = dir),
+        "`level`.*negative"
+      )
+    }
+  }
+}
+
+
+# Input data --------------------------------------------------------------
+cur_fin <- new_d(data.frame(x = 1:4, prob = (1:4) / 10), "fin")
+cur_infin <- new_d(data.frame(x = 0:1, y = c(1, 1)), "infin")
+
+
+# form_tails --------------------------------------------------------------
+test_that("form_tails works with `type='fin'` and `method='trim'`", {
+  # `direction = "both"`
+  # This should remove first row (not leave it with value 0 at "prob" column)
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.1, "trim", "both"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.2, 0.3, 0.3) / 0.8)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.15, "trim", "both"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.15, 0.3, 0.25) / 0.7)
+  )
+
+  # `direction = "left"`
+  # This should remove first row (not leave it with value 0 at "prob" column)
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.1, "trim", "left"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.2, 0.3, 0.4) / 0.9)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.15, "trim", "left"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.15, 0.3, 0.4) / 0.85)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.8, "trim", "left"))[, c("x", "prob")],
+    data.frame(x = 4, prob = 1)
+  )
+
+  # `direction = "right"`
+  # This should remove last row (not leave it with value 0 at "prob" column)
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.4, "trim", "right"))[, c("x", "prob")],
+    data.frame(x = 1:3, prob = c(0.1, 0.2, 0.3) / 0.6)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.45, "trim", "right"))[, c("x", "prob")],
+    data.frame(x = 1:3, prob = c(0.1, 0.2, 0.25) / 0.55)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.8, "trim", "right"))[, c("x", "prob")],
+    data.frame(x = 1:2, prob = c(0.5, 0.5))
+  )
+})
+
+test_that("form_tails works with `type='infin'` and `method='trim'`", {
+  # `direction = "both"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.05, "trim", "both"))[, c("x", "y")],
+    data.frame(x = c(0.05, 0.95), y = c(1, 1) / 0.9)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.4, "trim", "both"))[, c("x", "y")],
+    data.frame(x = c(0.4, 0.6), y = c(1, 1) / 0.2)
+  )
+
+  # `direction = "left"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.05, "trim", "left"))[, c("x", "y")],
+    data.frame(x = c(0.05, 1), y = c(1, 1) / 0.95)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.8, "trim", "left"))[, c("x", "y")],
+    data.frame(x = c(0.8, 1), y = c(1, 1) / 0.2)
+  )
+
+  # `direction = "right"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.05, "trim", "right"))[, c("x", "y")],
+    data.frame(x = c(0, 0.95), y = c(1, 1) / 0.95)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.8, "trim", "right"))[, c("x", "y")],
+    data.frame(x = c(0, 0.2), y = c(1, 1) / 0.2)
+  )
+})
+
+test_that("form_tails works with `type='fin'` and `method='winsor'`", {
+  # `direction = "both"`
+  # Here first row ISN'T removed (unlike with "trim") because 10% quantile is 1
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.1, "winsor", "both"))[, c("x", "prob")],
+    data.frame(x = 1:4, prob = (1:4) / 10)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.11, "winsor", "both"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.3, 0.3, 0.4))
+  )
+
+  # `direction = "left"`
+  # Here first row ISN'T removed (unlike with "trim") because 10% quantile is 1
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.1, "winsor", "left"))[, c("x", "prob")],
+    data.frame(x = 1:4, prob = (1:4) / 10)
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.11, "winsor", "left"))[, c("x", "prob")],
+    data.frame(x = 2:4, prob = c(0.3, 0.3, 0.4))
+  )
+
+  # `direction = "right"`
+  # Here last row IS removed because 60% (100-40) quantile is 3
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.4, "winsor", "right"))[, c("x", "prob")],
+    data.frame(x = 1:3, prob = c(0.1, 0.2, 0.7))
+  )
+  expect_equal(
+    meta_x_tbl(form_tails(cur_fin, 0.41, "winsor", "right"))[, c("x", "prob")],
+    data.frame(x = 1:3, prob = c(0.1, 0.2, 0.7))
+  )
+})
+
+test_that("form_tails works with `type='infin'` and `method='winsor'`", {
+  # `direction = "both"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.1, "winsor", "both"))[, c("x", "y")],
+    data.frame(
+      x = c(  0.1, 0.1+1e-8, 0.9-1e-8,   0.9),
+      # Here `2e7+1` is used instead of `2e7` due to (seems like) numerical
+      # representation issues
+      y = c(2e7+1,        1,        1, 2e7+1)
+    )
+  )
+
+  # `direction = "left"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.1, "winsor", "left"))[, c("x", "y")],
+    data.frame(
+      x = c(  0.1, 0.1+1e-8, 1),
+      # Here `2e7+1` is used instead of `2e7` due to (seems like) numerical
+      # representation issues
+      y = c(2e7+1,        1, 1)
+    )
+  )
+
+  # `direction = "right"`
+  expect_equal(
+    meta_x_tbl(form_tails(cur_infin, 0.1, "winsor", "right"))[, c("x", "y")],
+    data.frame(
+      x = c(0, 0.9-1e-8,   0.9),
+      # Here `2e7+1` is used instead of `2e7` due to (seems like) numerical
+      # representation issues
+      y = c(1,        1, 2e7+1)
+    )
+  )
+})
+
+test_that("form_tails returns self when `level = 0`", {
+  expect_self_x_tbl(cur_fin)
+  expect_self_x_tbl(cur_infin)
+})
+
+test_that("form_tails returns dirac distribution at maximum level", {
+  expect_dirac(cur_fin, c("both" = 3, "left" = 4, "right" = 1))
+  expect_dirac(cur_infin, c("both" = 0.5, "left" = 1, "right" = 0))
+})
+
+test_that("form_tails asserts bad input",  {
+  expect_error(form_tails("a", 0.1), "`f`.*function")
+  expect_error(form_tails(function(x) {x}, 0.1), "`f`.*pdqr")
+  expect_error(form_tails(cur_fin, "a"), "`level`.*single number")
+  expect_error(form_tails(cur_fin, 0.1, method = 1), "`method`.*string")
+  expect_error(form_tails(cur_fin, 0.1, method = "a"), "`method`.*one of")
+  expect_error(form_tails(cur_fin, 0.1, direction = 1), "`direction`.*string")
+  expect_error(form_tails(cur_fin, 0.1, direction = "a"), "`direction`.*one of")
+
+  # Tests for negative `level`
+  expect_error_negative_level(cur_fin)
+
+  # Tests for bigger `level` than maximum for particular `direction`
+  expect_error(form_tails(cur_fin, 0.6, "trim", "both"), "`level`.*0.5")
+  expect_error(form_tails(cur_fin, 0.6, "winsor", "both"), "`level`.*0.5")
+  expect_error(form_tails(cur_fin, 1.2, "trim", "left"), "`level`.*1")
+  expect_error(form_tails(cur_fin, 1.2, "winsor", "left"), "`level`.*1")
+  expect_error(form_tails(cur_fin, 1.2, "trim", "right"), "`level`.*1")
+  expect_error(form_tails(cur_fin, 1.2, "winsor", "right"), "`level`.*1")
+})
+
+
+# tails_trim --------------------------------------------------------------
+# Tested in `form_tails()`
+
+
+# tails_winsor ------------------------------------------------------------
+# Tested in `form_tails()`
+
+
+# tails_trim_fin ----------------------------------------------------------
+# Tested in `form_tails()`
+
+
+# tails_trim_infin --------------------------------------------------------
+# Tested in `form_tails()`
+
+
+# assert_form_tails_args --------------------------------------------------
+# Tested in `form_tails()`
+
+
+# trim_all ----------------------------------------------------------------
+# Tested in `form_tails()`
+
+
+# compute_support_after_remove --------------------------------------------
+# Tested in `form_tails()`
+
+
+# decrease_row_prob -------------------------------------------------------
+# Tested in `form_tails()`
