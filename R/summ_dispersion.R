2408dc408abfec5f4255cf7595eb8142086deeb7
echasnovski <evgeni.chasnovski@gmail.com>
2019-03-31 19:03:05 +0300

    Implement `summ_sd()`.

--- R/summ_dispersion.R
+++ R/summ_dispersion.R
@@ -3,3 +3,8 @@ summ_var <- function(f) {
   # `max(*, 0)` is used to take into account numerical representation accuracy
   max(-summ_mean(f)^2 + raw_moment(f, order = 2), 0)
 }
+
+summ_sd <- function(f) {
+  # `f` is validated inside `summ_var()`
+  sqrt(summ_var(f))
+}
--- tests/testthat/test-summ_dispersion.R
+++ tests/testthat/test-summ_dispersion.R
@@ -59,3 +59,57 @@ test_that("summ_var validates input", {
   expect_error(summ_var("a"), "`f`.*function")
   expect_error(summ_var(function(x) {x}), "`f`.*pdqr")
 })
+
+
+# summ_sd -----------------------------------------------------------------
+test_that("summ_sd works with 'fin' functions", {
+  expect_equal_stat(summ_sd, stat_list[["binom"]], "sd")
+
+  # Output isn't exact because of tail trimming during `as_d()`
+  expect_equal_stat(summ_sd, stat_list[["pois"]], "sd", thres = 1e-4)
+})
+
+test_that("summ_sd works with common 'infin' functions", {
+  expect_equal_stat(summ_sd, stat_list[["beta"]], "sd", thres = 5e-6)
+  expect_equal_stat(summ_sd, stat_list[["beta_inf"]], "sd", thres = 1e-3)
+  expect_equal_stat(summ_sd, stat_list[["chisq"]], "sd", thres = 5e-4)
+  # Big threshold because original density goes to infinity at left edge
+  expect_equal_stat(summ_sd, stat_list[["chisq_inf"]], "sd", thres = 1e-2)
+  expect_equal_stat(summ_sd, stat_list[["exp"]], "sd", thres = 2e-4)
+  expect_equal_stat(summ_sd, stat_list[["norm"]], "sd", thres = 5e-5)
+  expect_equal_stat(summ_sd, stat_list[["norm_2"]], "sd", thres = 5e-6)
+  expect_equal_stat(summ_sd, stat_list[["unif"]], "sd")
+})
+
+test_that("summ_sd works with dirac-like 'infin' functions", {
+  d_dirac <- new_d(2, "infin")
+  expect_equal(summ_sd(d_dirac), 0)
+
+  d_dirac_2 <- form_mix(
+    lapply(1:2, new_d, type = "infin"), weights = c(0.7, 0.3)
+  )
+  expect_equal(summ_sd(d_dirac_2), sqrt(1^2*0.7+2^2*0.3 - 1.3^2))
+})
+
+test_that("summ_sd works with winsorized 'infin' functions", {
+  d_wins <- form_resupport(
+    new_d(data.frame(x = 0:1, y = c(1, 1)), "infin"),
+    support = c(0.25, 0.85),
+    method = "winsor"
+  )
+  expect_equal(summ_sd(d_wins), sqrt(0.0531))
+})
+
+test_that("summ_sd works with 'infin' functions with few intervals", {
+  d_unif_1 <- new_d(data.frame(x = 1:2, y = c(1, 1)), "infin")
+  expect_equal(summ_sd(d_unif_1), sqrt(1/12))
+
+  d_unif_2 <- new_d(data.frame(x = 0:2, y = c(1, 1, 1)/2), "infin")
+  expect_equal(summ_sd(d_unif_2), sqrt(1/3))
+})
+
+test_that("summ_sd validates input", {
+  expect_error(summ_sd("a"), "`f`.*function")
+  expect_error(summ_sd(function(x) {x}), "`f`.*pdqr")
+})
+
