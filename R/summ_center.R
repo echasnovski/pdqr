ff91c7ba06ccb566da365f9a7cd590d2a5c20497
echasnovski <evgeni.chasnovski@gmail.com>
2019-03-31 12:35:40 +0300

    Implement `summ_mode()`.

--- R/summ_center.R
+++ R/summ_center.R
@@ -17,6 +17,29 @@ summ_median <- function(f) {
   as_q(f)(0.5)
 }
 
+summ_mode <- function(f, method = "global") {
+  assert_pdqr_fun(f)
+  assert_type(method, is_string)
+  assert_in_set(method, c("global", "local"))
+
+  f_x_tbl <- meta_x_tbl(f)
+  x <- f_x_tbl[["x"]]
+  col_name <- switch(meta_type(f), fin = "prob", infin = "y")
+  col <- f_x_tbl[[col_name]]
+
+  if (method == "global") {
+    # Returns the first (smallest) value if there are more than 1
+    x[which.max(col)]
+  } else {
+    col_left <- col[-length(col)]
+    col_right <- col[-1]
+    col_geq_right <- c(col_left >= col_right, TRUE)
+    col_geq_left <- c(TRUE, col_right >= col_left)
+
+    x[col_geq_right & col_geq_left]
+  }
+}
+
 summ_mean_infin <- function(x_tbl) {
   n <- nrow(x_tbl)
   x_lag <- x_tbl[["x"]][-n]
--- tests/testthat/test-summ_center.R
+++ tests/testthat/test-summ_center.R
@@ -1,6 +1,14 @@
 context("test-summ_center")
 
 
+# Custom expectations -----------------------------------------------------
+expect_local_mode_equals_global <- function(d_fun) {
+  expect_equal(
+    summ_mode(d_fun, method = "local"), summ_mode(d_fun, method = "global")
+  )
+}
+
+
 # summ_mean ---------------------------------------------------------------
 test_that("summ_mean works with 'fin' functions", {
   expect_equal_stat(summ_mean, stat_list[["binom"]], "mean")
@@ -142,6 +150,113 @@ test_that("summ_median works with 'infin' functions with few intervals", {
 test_that("summ_median asserts bad input", {
   expect_error(summ_median("a"), "`f`.*function")
   expect_error(summ_median(function(x) {x}), "`f`.*pdqr")
+})
+
+
+# summ_mode ---------------------------------------------------------------
+test_that("summ_mode works with 'fin' functions", {
+  # Method "global" (default)
+  expect_equal_stat(summ_mode, stat_list[["binom"]], "mode", thres = 1e-12)
+  expect_equal_stat(summ_mode, stat_list[["pois"]], "mode", thres = 1e-12)
+
+  # Method "local"
+  expect_local_mode_equals_global(stat_list[["binom"]][["d_fun"]])
+    # Poisson distribution's number of local modes depends on "integerishness"
+    # of `labmda` parameter
+  expect_equal(summ_mode(as_d(dpois, lambda = 10.1), method = "local"), 10)
+  expect_equal(summ_mode(as_d(dpois, lambda = 10), method = "local"), c(9, 10))
+})
+
+test_that("summ_mode works with common 'infin' functions", {
+  # Method "global" (default)
+  expect_equal_stat(summ_mode, stat_list[["beta"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["beta_inf"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["chisq"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["chisq_inf"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["exp"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["norm"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["norm_2"]], "mode")
+  expect_equal_stat(summ_mode, stat_list[["unif"]], "mode")
+
+  # Method "local"
+  expect_local_mode_equals_global(stat_list[["beta"]][["d_fun"]])
+    # Both "infinities" are local modes
+  expect_equal(
+    summ_mode(stat_list[["beta_inf"]][["d_fun"]], method = "local"),
+    c(0, 1)
+  )
+  expect_local_mode_equals_global(stat_list[["chisq"]][["d_fun"]])
+  expect_local_mode_equals_global(stat_list[["chisq_inf"]][["d_fun"]])
+  expect_local_mode_equals_global(stat_list[["exp"]][["d_fun"]])
+  expect_local_mode_equals_global(stat_list[["norm"]][["d_fun"]])
+  expect_local_mode_equals_global(stat_list[["norm_2"]][["d_fun"]])
+    # All points in "x_tbl" are local modes
+  expect_equal(
+    summ_mode(stat_list[["unif"]][["d_fun"]], method = "local"),
+    meta_x_tbl(stat_list[["unif"]][["d_fun"]])[["x"]]
+  )
+})
+
+test_that("summ_mode works with dirac-like 'infin' functions", {
+  d_dirac <- new_d(2, "infin")
+  d_dirac_2 <- form_mix(
+    lapply(1:2, new_d, type = "infin"), weights = c(0.7, 0.3)
+  )
+
+  # Method "global" (default)
+  expect_equal(summ_mode(d_dirac), 2)
+  expect_equal(summ_mode(d_dirac_2), 1)
+
+  # Method "local"
+  expect_equal(summ_mode(d_dirac, method = "local"), 2)
+  expect_equal(summ_mode(d_dirac_2, method = "local"), c(1, 2))
+})
+
+test_that("summ_mode works with winsorized 'infin' functions", {
+  # Method "global" (default)
+  d_wins <- form_resupport(
+    new_d(data.frame(x = 0:1, y = c(1, 1)), "infin"),
+    support = c(0.25, 0.85),
+    method = "winsor"
+  )
+  expect_equal(summ_mode(d_wins), 0.25)
+
+  # Method "local"
+  d_norm_wins <- form_resupport(as_d(dnorm), c(-1, 2), method = "winsor")
+  expect_equal(summ_mode(d_norm_wins, method = "local"), c(-1, 0, 2))
+})
+
+test_that("summ_mode works with plateaus in distribution", {
+  d_plateau_fin <- new_d(
+    data.frame(x = 1:5, prob = c(0.15, 0.2, 0.2, 0.2, 0.25)), "fin"
+  )
+  d_plateau_fin_2 <- new_d(
+    data.frame(x = 1:5, prob = c(0.1, 0.25, 0.25, 0.25, 0.15)), "fin"
+  )
+  # `y` isn't exact and will get renormalized
+  d_plateau_infin <- new_d(data.frame(x = 1:5, y = c(1, 2, 2, 2, 3)), "infin")
+  d_plateau_infin_2 <- new_d(data.frame(x = 1:5, y = c(1, 2, 2, 2, 1)), "infin")
+
+  # Method "global" (default)
+  expect_equal(summ_mode(d_plateau_fin), 5)
+    # Returns the smallest "x" with highest probability
+  expect_equal(summ_mode(d_plateau_fin_2), 2)
+  expect_equal(summ_mode(d_plateau_infin), 5)
+    # Returns the smallest "x" with highest probability
+  expect_equal(summ_mode(d_plateau_infin_2), 2)
+
+  # Method "local"
+  expect_equal(summ_mode(d_plateau_fin, method = "local"), c(2, 3, 5))
+  expect_equal(summ_mode(d_plateau_fin_2, method = "local"), c(2, 3, 4))
+  expect_equal(summ_mode(d_plateau_infin, method = "local"), c(2, 3, 5))
+  expect_equal(summ_mode(d_plateau_infin_2, method = "local"), c(2, 3, 4))
+})
+
+test_that("summ_mode asserts bad input", {
+  expect_error(summ_mode("a"), "`f`.*function")
+  expect_error(summ_mode(function(x) {x}), "`f`.*pdqr")
+  expect_error(summ_mode(d_fin, method = 1), "`method`.*string")
+  expect_error(summ_mode(d_fin, method = "a"), "`method`.*one of")
 })
 
 
