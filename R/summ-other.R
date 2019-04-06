df08a1cc8032cfd05acf46bce390edcb5c050cc4
echasnovski <evgeni.chasnovski@gmail.com>
2019-04-06 11:23:05 +0300

    Implement `summ_prob_true()` and `summ_prob_false()`.

--- R/summ-other.R
+++ R/summ-other.R
@@ -7,3 +7,21 @@ summ_quantile <- function(f, probs) {
 
   as_q(f)(probs)
 }
+
+summ_prob_true <- function(f) {
+  assert_pdqr_fun(f)
+  if (!is_boolean_pdqr_fun(f)) {
+    warning_collapse(
+      '`f` is not a "boolean" pdqr-function (type "fin" with `x` in "x_tbl" ',
+      'identical to c(0, 1)). Proceed with caution.'
+    )
+  }
+
+  x_tbl <- meta_x_tbl(f)
+
+  x_tbl[["prob"]][x_tbl[["x"]] == 1]
+}
+
+summ_prob_false <- function(f) {
+  1 - summ_prob_true(f)
+}
--- tests/testthat/test-summ-other.R
+++ tests/testthat/test-summ-other.R
@@ -19,3 +19,47 @@ test_that("summ_quantile validates input", {
   expect_error(summ_quantile(d_fin, c(-1e-6, 0)), "`probs`.*\\[0; 1\\]")
   expect_error(summ_quantile(d_fin, c(1, 1+1e-6)), "`probs`.*\\[0; 1\\]")
 })
+
+
+# summ_prob_true ----------------------------------------------------------
+test_that("summ_prob_true works", {
+  cur_bool_1 <- new_d(data.frame(x = c(0, 1), prob = c(0.3, 0.7)), "fin")
+  expect_equal(summ_prob_true(cur_bool_1), 0.7)
+
+  cur_bool_2 <- new_d(data.frame(x = c(0, 1), prob = c(1, 0)), "fin")
+  expect_equal(summ_prob_true(cur_bool_2), 0)
+})
+
+test_that("summ_prob_true warns about non-boolean pdqr-function", {
+  expect_warning(summ_prob_true(new_d(-1, "fin")), "not.*boolean")
+  # "Boolean" pdqr-function should have both 0 and 1 in `x`
+  expect_warning(summ_prob_true(new_d(0, "fin")), "not.*boolean")
+  expect_warning(summ_prob_true(new_d(1, "fin")), "not.*boolean")
+})
+
+test_that("summ_prob_true validates input", {
+  expect_error(summ_prob_true("a"), "`f`.*function")
+  expect_error(summ_prob_true(function(x) {x}), "`f`.*pdqr")
+})
+
+
+# summ_prob_false ---------------------------------------------------------
+test_that("summ_prob_false works", {
+  cur_bool_1 <- new_d(data.frame(x = c(0, 1), prob = c(0.3, 0.7)), "fin")
+  expect_equal(summ_prob_false(cur_bool_1), 0.3)
+
+  cur_bool_2 <- new_d(data.frame(x = c(0, 1), prob = c(1, 0)), "fin")
+  expect_equal(summ_prob_false(cur_bool_2), 1)
+})
+
+test_that("summ_prob_false warns about non-boolean pdqr-function", {
+  expect_warning(summ_prob_false(new_d(-1, "fin")), "not.*boolean")
+  # "Boolean" pdqr-function should have both 0 and 1 in `x`
+  expect_warning(summ_prob_false(new_d(0, "fin")), "not.*boolean")
+  expect_warning(summ_prob_false(new_d(1, "fin")), "not.*boolean")
+})
+
+test_that("summ_prob_false validates input", {
+  expect_error(summ_prob_false("a"), "`f`.*function")
+  expect_error(summ_prob_false(function(x) {x}), "`f`.*pdqr")
+})
