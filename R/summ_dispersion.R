67f01a645376af6ca3b92e7a0b563e8d8c131c9c
echasnovski <evgeni.chasnovski@gmail.com>
2019-04-07 14:15:10 +0300

    Change order of `summ_var()` and `summ_sd()`.

--- R/summ_dispersion.R
+++ R/summ_dispersion.R
@@ -60,17 +60,17 @@ summ_dispersion <- function(f, method = "sd") {
 
 #' @rdname summ_dispersion
 #' @export
-summ_var <- function(f) {
-  # `f` is validated inside `summ_mean()`
-  # `max(*, 0)` is used to take into account numerical representation accuracy
-  max(-summ_mean(f)^2 + raw_moment(f, order = 2), 0)
+summ_sd <- function(f) {
+  # `f` is validated inside `summ_var()`
+  sqrt(summ_var(f))
 }
 
 #' @rdname summ_dispersion
 #' @export
-summ_sd <- function(f) {
-  # `f` is validated inside `summ_var()`
-  sqrt(summ_var(f))
+summ_var <- function(f) {
+  # `f` is validated inside `summ_mean()`
+  # `max(*, 0)` is used to take into account numerical representation accuracy
+  max(-summ_mean(f)^2 + raw_moment(f, order = 2), 0)
 }
 
 #' @rdname summ_dispersion
