86e449251e97ddcc490745e0a59a42aa8689218d
echasnovski <evgeni.chasnovski@gmail.com>
2019-04-29 22:26:51 +0300

    Implement `summ_hdr()`.

--- R/summ_hdr.R
+++ R/summ_hdr.R
@@ -1,3 +1,148 @@
+# Note about nature of output:
+# - It is approximate and "HDR" probability can even be lower than `level`.
+# - HDR probability can be far bigger than `level` in case plateaus are present.
+#   However, this seems to align with definition of HDR: `{x | f(x) >= f_a}`
+#   where `f_a` is the largest value such that `P({x | f(x) >= f_a}) >= level`.
+#   Definition is from "Computing and Graphing Highest Density Regions" by Rob
+#   J. Hyndman.
+summ_hdr <- function(f, level = 0.95) {
+  assert_pdqr_fun(f)
+  assert_type(
+    level, is_single_number,
+    type_name = "single number between 0 and 1",
+    min_val = 0, max_val = 1
+  )
+
+  # Early returns for special `level`s
+  if (level == 0) {
+    f_mode <- summ_mode(f, method = "global")
+
+    return(data.frame(left = f_mode, right = f_mode))
+  }
+  if (level == 1) {
+    f_supp <- meta_support(f)
+
+    return(data.frame(left = f_supp[1], right = f_supp[2]))
+  }
+
+  switch(
+    meta_type(f),
+    fin = hdr_fin(f, level),
+    infin = hdr_infin(f, level)
+  )
+}
+
+hdr_fin <- function(f, level) {
+  x_tbl <- meta_x_tbl(f)
+  x <- x_tbl[["x"]]
+  prob <- x_tbl[["prob"]]
+  prob_order <- order(prob, decreasing = TRUE)
+
+  # Compute index of ordered decreacingly `x` at which total probability exceeds
+  # `level`.
+  height_ind <- which(cumsum(prob[prob_order]) >= level)[1]
+  prob_is_high <- prob >= prob[prob_order][height_ind]
+  high_inds <- which(prob_is_high)
+  x_high <- x[prob_is_high]
+
+  # Squash `high_inds` into intervals of consecutive indicies
+  ind_is_jump <- (high_inds[-1] - high_inds[-length(high_inds)]) != 1
+  is_interval_left <- c(TRUE, ind_is_jump)
+  is_interval_right <- c(ind_is_jump, TRUE)
+
+  data.frame(left = x_high[is_interval_left], right = x_high[is_interval_right])
+}
+
+hdr_infin <- function(f, level) {
+  # "Cut down" `y` values inside very narrow intervals. Otherwise they might
+  # greatly skew computation of target height. This happen in winsorized
+  # pdqr-functions.
+  cut_dd <- cut_down_density(f, level)
+
+  # Compute height which gives desired HDR
+  target_height <- compute_target_height(cut_dd[["x_tbl"]], cut_dd[["level"]])
+
+  # Computation of intervals should be made with original `f`, and not with its
+  # "cut down" version
+  compute_hdr_intervals(f, target_height)
+}
+
+cut_down_density <- function(f, level) {
+  # This computes `y` range excluding those values inside "very narrow"
+  # intervals, which are (very probably) dirac-like values.
+  y_lim <- compute_d_infin_ylim(f)
+  x_tbl <- meta_x_tbl(f)
+  y <- x_tbl[["y"]]
+  y_cut_down <- pmin(y, y_lim[2])
+
+  # Proceed based upon how much of probability might be cut out when `y` values
+  # are "cut down" to range computed earlier.
+  exclude_prob <- 1 - trapez_integral(x_tbl[["x"]], y_cut_down)
+  if (exclude_prob > level) {
+    # If possible cut is bigger than `level`, act only on "very narrow"
+    # intervals, because they contain the needed HDR
+    x_tbl[["y"]][y <= y_lim[2]] <- 0
+  } else {
+    # If possible cut is smaller than `level`, act on the rest of intervals
+    # while adjusting target `level`
+    x_tbl[["y"]] <- y_cut_down
+    level <- (level - exclude_prob) / (1 - exclude_prob)
+  }
+
+  list(x_tbl = x_tbl[, c("x", "y")], level = level)
+}
+
+compute_target_height <- function(x_tbl, level) {
+  # Target height for HDR with level `level` is computed as `1-level` quantile
+  # of random variable `Y = d_f(X)` (`d_f` - probability density of input random
+  # variable, `X` - input random variable represented by pdqr-function `f`).
+  # `Y`s CDF is approximated by computing density values `y` at certain grid and
+  # assuming that density of `Y` at point `y` is proportional to `y` (because it
+  # is a value of `X`'s density, i.e. transformation function). Grid is created
+  # to be a union of all `x` values from `f`'s "x_tbl" and other `x`s
+  # corresponding to non-zero `f`'s density. Zero probability intervals are not
+  # interesting in case of transformation of random variable (also using them
+  # introduces errors if they obtain much of the support).
+  # Another way of approximate this quantile is to use simulation, but it is
+  # a random approach which is not desirable:
+  #   smpl <- as_r(f)(n_sample)
+  #   quantile(as_d(f)(smpl), probs = 1 - level)
+
+  d_f <- enfun_x_tbl(x_tbl)
+
+  # Here `unique()` is used to not "over-sample" `y` values
+  x_vec <- unique(c(x_tbl[["x"]], grid_on_pos_prob(x_tbl, n_grid = 10001)))
+  y_vec <- sort(d_f(x_vec))
+  quan_ind <- findInterval(
+    1-level, cumsum(y_vec) / sum(y_vec),
+    # `all.inside = TRUE` is needed to ensure that too small values of `1-level`
+    # won't cause `quan_ind` equal to 0
+    all.inside = TRUE
+  )
+
+  y_vec[quan_ind]
+}
+
+grid_on_pos_prob <- function(x_tbl, n_grid) {
+  n <- nrow(x_tbl)
+  x <- x_tbl[["x"]]
+  x_l <- x[-n]
+  x_r <- x[-1]
+  y <- x_tbl[["y"]]
+
+  pos_prob <- !((y[-n] == 0) & (y[-1] == 0))
+  x_l <- x_l[pos_prob]
+  x_r <- x_r[pos_prob]
+
+  # Treat all intervals with positive probability as one consecutive
+  # ("squashed") interval and produce sequence with desired number of elements
+  width_vec <- cumsum(x_r - x_l)
+  res_width <- seq(0, width_vec[length(width_vec)], length.out = n_grid)
+  inter_ind <- findInterval(res_width, width_vec, rightmost.closed = TRUE) + 1
+
+  x_r[inter_ind] - (width_vec[inter_ind] - res_width)
+}
+
 compute_hdr_intervals <- function(f, height) {
   f_supp <- meta_support(f)
   # `x_height` has edges of intervals **with positive width** inside which
@@ -10,7 +155,7 @@ compute_hdr_intervals <- function(f, height) {
 
   # Determining which intervals are `>= height`
   intervals_center <- 0.5*(x_height[-1] + x_height[-n])
-  interval_is_high <- f(intervals_center) >= height
+  interval_is_high <- as_d(f)(intervals_center) >= height
   high_inds <- which(interval_is_high)
   x_l <- x_height[high_inds]
   x_r <- x_height[high_inds + 1]
--- tests/testthat/test-summ_hdr.R
+++ tests/testthat/test-summ_hdr.R
@@ -1,8 +1,246 @@
-context("summ_hdr")
+context("test-summ_hdr")
 
 
 # Helper data -------------------------------------------------------------
 empty_hdr <- data.frame(left = numeric(0), right = numeric(0))
+
+
+# summ_hdr ----------------------------------------------------------------
+test_that("summ_hdr works with 'fin' functions", {
+  cur_d_1 <- new_d(data.frame(x = 1:4, prob = 1:4/10), "fin")
+  expect_equal(summ_hdr(cur_d_1, 0.1), data.frame(left = 4, right = 4))
+  expect_equal(summ_hdr(cur_d_1, 0.4), data.frame(left = 4, right = 4))
+  expect_equal(summ_hdr(cur_d_1, 0.4001), data.frame(left = 3, right = 4))
+  expect_equal(summ_hdr(cur_d_1, 0.95), data.frame(left = 1, right = 4))
+
+  cur_d_2 <- new_d(data.frame(x = 1:4, prob = c(0.4, 0.2, 0.3, 0.1)), "fin")
+  expect_equal(summ_hdr(cur_d_2, 0.1), data.frame(left = 1, right = 1))
+  expect_equal(
+    summ_hdr(cur_d_2, 0.4001), data.frame(left = c(1, 3), right = c(1, 3))
+  )
+  expect_equal(summ_hdr(cur_d_2, 0.9), data.frame(left = 1, right = 3))
+  expect_equal(summ_hdr(cur_d_2, 0.95), data.frame(left = 1, right = 4))
+})
+
+test_that("summ_hdr works with basic 'infin' functions", {
+  # Here some edges of output aren't exact because computation of target height
+  # isn't exact
+
+  # Single mode at support edge
+  cur_d_1 <- new_d(data.frame(x = c(0, 2), y = c(0, 1)), "infin")
+
+  hdr_1 <- summ_hdr(cur_d_1, level = 0.1)
+  expect_equal(hdr_1[["left"]], as_q(cur_d_1)(0.9), tolerance = 1e-4)
+  expect_equal(hdr_1[["right"]], 2)
+
+  # Single mode in the middle
+  cur_d_2 <- new_d(data.frame(x = 0:2, y = c(0, 1, 0)), "infin")
+  hdr_2_1 <- summ_hdr(cur_d_2, 0.4)
+  left_q_1 <- as_q(cur_d_2)(0.3)
+  expect_equal(hdr_2_1[["left"]], left_q_1, tolerance = 3e-4)
+  expect_equal(hdr_2_1[["right"]], 2 - left_q_1, tolerance = 3e-4)
+
+  expect_equal(summ_hdr(cur_d_2, 0.75), data.frame(left = 0.5, right = 1.5))
+
+  # Several global modes
+  cur_d_3 <- new_d(data.frame(x = 0:6, y = c(0, 1, 0, 1, 0, 1, 0)/3), "infin")
+  expect_equal(
+    summ_hdr(cur_d_3, 0.75),
+    data.frame(left = c(0.5, 2.5, 4.5), right = c(1.5, 3.5, 5.5))
+  )
+
+  # Several local modes
+  cur_d_4 <- new_d(data.frame(x = 1:5, y = c(1, 0, 2, 0, 1)/3), "infin")
+    # Here target height is equal to density at local edge modes (at 1 and 5).
+    # The output should have only intervals of non-zero width, i.e. no `[1; 1]`
+    # and no `[5; 5]`.
+  expect_equal(summ_hdr(cur_d_4, 0.5), data.frame(left = 2.5, right = 3.5))
+})
+
+test_that("summ_hdr works with extreme 'infin' functions", {
+  # Wide zero probability interval. Tests adequacy of `compute_target_height()`
+  zero_left <- new_d(
+    data.frame(x = c(1, 100000+0:2), y = c(0, 0, 1, 0)), "infin"
+  )
+  expect_equal(
+    summ_hdr(zero_left, level = 0.75),
+    data.frame(left = 100000.5, right = 100001.5)
+  )
+
+  zero_mid <- new_d(
+    data.frame(x = c(1:3, 100000+0:2), y = c(0, 1, 0, 0, 1, 0)), "infin"
+  )
+  expect_equal(
+    summ_hdr(zero_mid, level = 0.75),
+    data.frame(left = c(1.5, 100000.5), right = c(2.5, 100001.5))
+  )
+
+  zero_right <- new_d(
+    data.frame(x = c(1:3, 100000), y = c(0, 1, 0, 0)), "infin"
+  )
+  expect_equal(
+    summ_hdr(zero_right, level = 0.75), data.frame(left = 1.5, right = 2.5)
+  )
+
+  # Infinite values in original density
+  d_beta <- as_d(dbeta, shape1 = 0.5, shape2 = 0.5)
+  left_q_beta <- as_q(d_beta)(0.3)
+  hdr_beta <- summ_hdr(d_beta, 0.6)
+  expect_equal(hdr_beta[["left"]], c(0, 1 - left_q_beta), tolerance = 2e-3)
+  expect_equal(hdr_beta[["right"]], c(left_q_beta, 1), tolerance = 2e-3)
+})
+
+test_that("summ_hdr works with dirac-like functions", {
+  # Type "fin"
+  d_fin_dirac <- new_d(100, "fin")
+  expect_equal(summ_hdr(d_fin_dirac, 0.1), data.frame(left = 100, right = 100))
+  expect_equal(summ_hdr(d_fin_dirac, 0.9), data.frame(left = 100, right = 100))
+
+  # Type "infin"
+  d_dirac_1 <- new_d(1, "infin")
+  expect_equal(
+    summ_hdr(d_dirac_1, level = 0.99),
+    data.frame(left = 1 - 1e-8, right = 1 + 1e-8),
+    tolerance = 1e-9
+  )
+
+  d_dirac_2 <- form_mix(list(new_d(1, "infin"), new_d(2, "infin")))
+  expect_equal(
+    summ_hdr(d_dirac_2, level = 0.99),
+    data.frame(left = 1:2 - 1e-8, right = 1:2 + 1e-8),
+    tolerance = 1e-9
+  )
+})
+
+test_that("summ_hdr works with winsorized functions", {
+  # Generally, errors in HDRs of winsorized functions are big. Current
+  # hypothesis is that this happens because of numerical precision issues at
+  # dirac-like intervals.
+
+  cur_d <- new_d(data.frame(x = 0:2, y = c(0, 1, 0)), "infin")
+  winsor_quan <- as_q(cur_d)(c(0.1, 0.9))
+  d_winsor <- form_tails(
+    cur_d, level = 0.1, method = "winsor", direction = "both"
+  )
+  p_winsor <- as_p(d_winsor)
+
+  # Case when dirac-like intervals contain all desired probability
+  expect_equal(
+    summ_hdr(d_winsor, level = 0.05),
+    data.frame(
+      left = winsor_quan + c(0, -1e-8), right = winsor_quan + c(1e-8, 0)
+    ),
+    tolerance = 1e-7
+  )
+
+  # Case when output should have intervals other than dirac-like.
+  # For more robustness total probability is tested
+  hdr_winsor_2 <- summ_hdr(d_winsor, level = 0.9)
+  tot_prob_2 <- sum(
+    p_winsor(hdr_winsor_2[["right"]]) - p_winsor(hdr_winsor_2[["left"]])
+  )
+  expect_equal(tot_prob_2, 0.9, 8e-3)
+})
+
+test_that("summ_hdr handles plateaus", {
+  # Plateaus at target height result into more total probability of output HDR
+  # than requested
+
+  d_unif <- new_d(data.frame(x = 0:1, y = c(1, 1)), "infin")
+  hdr_unif <- data.frame(left = 0, right = 1)
+  expect_equal(summ_hdr(d_unif, 0.05), hdr_unif)
+  expect_equal(summ_hdr(d_unif, 0.95), hdr_unif)
+
+  d_plateau <- new_d(data.frame(x = 1:4, y = c(2, 1, 1, 2)/4), "infin")
+  # Here target height is equal to 1/4, the plateau height. So the output is
+  # equal to the whole support because there is no region with less total width
+  # and total probability not less than 0.75.
+  expect_equal(summ_hdr(d_plateau, 0.75), data.frame(left = 1, right = 4))
+  # However, if level is slightly (with respect to accuracy of target height
+  # computation) less than 0.75, then there are two intervals in output
+  expect_equal(nrow(summ_hdr(d_plateau, 0.75-1e-3)), 2)
+})
+
+test_that("summ_hdr works with real world cases", {
+  # Type "fin"
+  d_binom <- as_d(dbinom, size = 10, prob = 0.5)
+  ref_hdr_quan <- qbinom(c(0.025, 0.975), size = 10, prob = 0.5)
+  expect_equal(
+    summ_hdr(d_binom, level = 0.95),
+    data.frame(left = ref_hdr_quan[1], right = ref_hdr_quan[2])
+  )
+
+  # Type "infin"
+  d_norm <- as_d(dnorm, mean = 10, sd = 10)
+  ref_hdr_quan <- qnorm(c(0.025, 0.975), mean = 10, sd = 10)
+  hdr_norm <- summ_hdr(d_norm, level = 0.95)
+  # Outputs are not exact because `d_norm` is a pdqr-approximation to input
+  # normal distribution
+  expect_equal(hdr_norm[["left"]], ref_hdr_quan[1], tolerance = 5e-3)
+  expect_equal(hdr_norm[["right"]], ref_hdr_quan[2], tolerance = 5e-3)
+
+  d_unif <- as_d(dunif)
+  expect_equal(summ_hdr(d_unif, level = 0.95), data.frame(left = 0, right = 1))
+})
+
+test_that("summ_hdr accepts different pdqr classes", {
+  ref_hdr <- summ_hdr(d_infin, 0.95)
+  expect_equal(summ_hdr(p_infin, 0.95), ref_hdr)
+  expect_equal(summ_hdr(q_infin, 0.95), ref_hdr)
+  expect_equal(summ_hdr(r_infin, 0.95), ref_hdr)
+})
+
+test_that("summ_hdr works with special values of `level`", {
+  # Type "fin"
+  d_fin_mode <- summ_mode(d_fin, method = "global")
+  d_fin_supp <- meta_support(d_fin)
+  expect_equal(
+    summ_hdr(d_fin, 0), data.frame(left = d_fin_mode, right = d_fin_mode)
+  )
+  expect_equal(
+    summ_hdr(d_fin, 1), data.frame(left = d_fin_supp[1], right = d_fin_supp[2])
+  )
+
+  # Type "infin"
+  d_infin_mode <- summ_mode(d_infin, method = "global")
+  d_infin_supp <- meta_support(d_infin)
+  expect_equal(
+    summ_hdr(d_infin, 0), data.frame(left = d_infin_mode, right = d_infin_mode)
+  )
+  expect_equal(
+    summ_hdr(d_infin, 1),
+    data.frame(left = d_infin_supp[1], right = d_infin_supp[2])
+  )
+})
+
+test_that("summ_hdr validates input", {
+  expect_error(summ_hdr("a"), "`f`.*function")
+  expect_error(summ_hdr(function(x) {x}), "`f`.*pdqr")
+  expect_error(summ_hdr(d_fin, level = "a"), "`level`.*number")
+  expect_error(summ_hdr(d_fin, level = c(0.05, 0.1)), "`level`.*single")
+  expect_error(summ_hdr(d_fin, level = 1.1), "`level`.*between 0 and 1")
+  expect_error(summ_hdr(d_fin, level = -0.1), "`level`.*between 0 and 1")
+})
+
+
+# hdr_fin -----------------------------------------------------------------
+# Tested in `summ_hdr()`
+
+
+# hdr_infin ---------------------------------------------------------------
+# Tested in `summ_hdr()`
+
+
+# cut_down_density --------------------------------------------------------
+# Tested in `summ_hdr()`
+
+
+# compute_target_height ---------------------------------------------------
+# Tested in `summ_hdr()`
+
+
+# grid_on_pos_prob --------------------------------------------------------
+# Tested in `summ_hdr()`
 
 
 # compute_hdr_intervals ---------------------------------------------------
