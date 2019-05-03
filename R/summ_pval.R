c733204f64f857ea83db77f58ab8b5f4df3af9a0
echasnovski <evgeni.chasnovski@gmail.com>
2019-05-03 21:39:18 +0300

    Export and document `summ_pval()`.

--- R/summ_pval.R
+++ R/summ_pval.R
@@ -1,3 +1,56 @@
+#' Summarize distribution with p-value
+#'
+#' `summ_pval()` computes p-value(s) based on supplied distribution and observed
+#' value(s). There are several methods of computing p-values ("both", "right",
+#' and "left") as well as several types of multiple comparison adjustments
+#' (using on [stats::p.adjust()]).
+#'
+#' @inheritParams summ_mean
+#' @param obs Numeric vector of observed values to be used as threshold for
+#'   p-value. Can have multiple values, in which case output will be adjusted
+#'   for multiple comparisons with [p.adjust()][stats::p.adjust()].
+#' @param method Method representing direction of p-value computation. Should be
+#'   one of "both", "right", "left".
+#' @param adjust Adjustment method as `method` argument to `p.adjust()`.
+#'
+#' @details Method "both" for each element in `obs` computes two-sided p-value
+#' as `min(1, 2 * min(right_p_val, left_p_val))`, where `right_p_val` and
+#' `left_p_val` are right and left one-sided p-values (ones which are computed
+#' with "right" and "left" methods) of `obs`'s elements correspondingly.
+#'
+#' Method "right" for each element `x` of `obs` computes probability of `f >= x`
+#' being true (more strictly, of random variable, represented by `f`, being not
+#' less than `x`). This corresponds to right one-sided p-value.
+#'
+#' Method "left" for each element `x` of `obs` computes probability of `f <= x`,
+#' which is a left one-sided p-value.
+#'
+#' **Note** that by default multiple p-values in output are adjusted with
+#' `p.adjust(*, method = adjust)`. To not do any adjustment, use `adjust =
+#' "none"`.
+#'
+#' @return A numeric vector with the same length as `obs` representing
+#' corresponding p-values after possible adjustment for multiple comparisons.
+#'
+#' @examples
+#' # Type "fin"
+#' d_fin <- new_d(data.frame(x = 1:5, prob = c(1, 2, 3, 2, 1) / 9), "fin")
+#' summ_pval(d_fin, 3, method = "both")
+#' summ_pval(d_fin, 3, method = "right")
+#' summ_pval(d_fin, 3, method = "left")
+#'
+#' # Type "infin"
+#' d_norm <- as_d(dnorm)
+#' summ_pval(d_norm, 2, method = "both")
+#' summ_pval(d_norm, 2, method = "right")
+#' summ_pval(d_norm, 2, method = "left")
+#'
+#' # Adjustment is made for multiple observed values
+#' summ_pval(d_norm, seq(0, 2, by = 0.1))
+#'   # Use `adjust = "none"` for to not do any adjustment
+#' summ_pval(d_norm, seq(0, 2, by = 0.1), adjust = "none")
+#'
+#' @export
 summ_pval <- function(f, obs, method = "both", adjust = "holm") {
   assert_pdqr_fun(f)
   assert_type(obs, is.numeric)
