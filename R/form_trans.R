bf191bd5779605010ddc2ba49d74fe891b6e21b4
echasnovski <evgeni.chasnovski@gmail.com>
2019-05-09 17:18:44 +0300

    Update documentation of `form_trans()`.

--- R/form_trans.R
+++ R/form_trans.R
@@ -1,33 +1,83 @@
-# Notes in docs about `method = "bruteforce"`:
-# - It is very memory consuming.
-# - It is usually useful when type of output function is "fin", i.e. when all
-#   input functions are "fin" or when output of `trans` is logical. Also
-#   probabilities shouldn't be very small because they will be directly
-#   multiplied.
-
 #' Transform pdqr-function
 #'
-#' Perform a transformation of pdqr-function(s).
+#' Perform a transformation of pdqr-function(s) (which assumed to be
+#' independent).
 #'
-#' @param f_list A list consisting from pdqr-function(s) and/or number(s).
-#'   Should have at least one pdqr-function (see Details).
-#' @param f A pdqr-function.
+#' @param f_list A list consisting from pdqr-function(s) and/or single
+#'   number(s). Should have at least one pdqr-function (see Details).
 #' @param trans Transformation function. Should take as many (vectorized)
-#'   arguments as there are elements in `f_list`.
+#'   arguments as there are elements in `f_list` or a single argument for
+#'   `form_trans_self()`. Should return numeric or logical values.
 #' @param ... Extra arguments to `trans`.
 #' @param method Transformation method. One of "random" or "bruteforce".
 #' @param n_sample Number of elements to sample.
 #' @param args_new List of extra arguments for [new_*()][new_d()] to control
 #'   [density()].
+#' @param f A pdqr-function.
+#'
+#' @details `form_trans_self()` is a thin wrapper for `form_trans()` that
+#' accepts a single pdqr-function instead of a list of them.
+#'
+#' [Class][meta_class()] of output is chosen as class of first pdqr-function in
+#' `f_list`. [Type][meta_type()] of output is chosen to be "fin" in case all
+#' input pdqr-functions have "fin" type, and "infin" otherwise.
+#'
+#' Method "random" performs transformation using random generation of samples:
+#' - **Generates a sample of size `n_sample` from every element of `f_list`**
+#' (if element is single number, it is repeated `n_sample` times).
+#' - **Calls `trans`** with all generated samples (in order aligned with
+#' `f_list`). **Note** that output should be either numeric or logical and have
+#' `n_sample` elements (one for each combination of input values in "vectorized"
+#' fashion). So, for example, using `sum` directly is not possible as it returns
+#' only single number.
+#' - **Creates output pdqr-function**. If output is logical, probability of
+#' being true is estimated as share of `TRUE` in output, and boolean
+#' pdqr-function is created (type "fin" with "x" values equal to 0 and 1, and
+#' probabilities of being false and true respectively). If output is numeric,
+#' one of `new_*()` (suitable for output class) is called with arguments from
+#' `args_new` list.
+#'
+#' Method "bruteforce":
+#' - **[Retypes][form_retype()] input** pdqr-function to "fin"
+#' type (using "piecelin" method).
+#' - **Computes output for every combination of "x" values** (probability of
+#' which will be a product of corresponding probabilities).
+#' - **Creates pdqr-function of type "fin"** with suitable `new_*()` function.
+#' - **Possibly retypes to "infin" type** if output should have it (also with
+#' "piecelin" method).
+#'
+#' **Notes** about "bruteforce" method:
+#' - Its main advantage is that it is not random.
+#' - It may start to be very memory consuming very quickly.
+#' - It is usually useful when type of output function is "fin". In case of
+#' "infin" type, retyping from "fin" to "infin" might introduce big errors.
+#' - Used "fin" probabilities shouldn't be very small because they will be
+#' directly multiplied, which might cause numerical accuracy issues.
 #'
 #' @return A pdqr-function for transformed random variable.
+#'
+#' @seealso [Pdqr methods for S3 group generic
+#'   functions][methods-group-generic] for more accurate implementations of most
+#'   commonly used functions. Some of them are direct (without randomness) and
+#'   some of them use `form_trans()` with "random" method.
+#'
+#' [form_regrid()] to increase/decrease granularity of pdqr-functions for method
+#' "bruteforce".
 #'
 #' @examples
 #' # Default "random" transformation
 #' d_norm <- as_d(dnorm)
+#'   # More accurate result would give use of `+` directly with: d_norm + d_norm
 #' d_norm_2 <- form_trans(list(d_norm, d_norm), trans = `+`)
 #' plot(d_norm_2)
-#' lines(as_d(dnorm, sd = 2), col = "red")
+#' lines(as_d(dnorm, sd = sqrt(2)), col = "red")
+#'
+#'   # Input list can have single numbers
+#' form_trans(list(d_norm, 100), trans = `+`)
+#'
+#'   # Output of `trans` can be logical. Next example is random version of
+#'   # `d_norm >= 0`.
+#' form_trans(list(d_norm, 0), trans = `>=`)
 #'
 #' # Transformation with "bruteforce" method
 #' power <- function(x, n = 1) {x^n}
@@ -40,6 +90,9 @@
 #'   # Compare with "random" method
 #' p_fin_sq_rand <- form_trans_self(p_fin, trans = power, n = 2)
 #' meta_x_tbl(p_fin_sq_rand)
+#'
+#' # `form_trans_self()` is a wrapper for `form_trans()`
+#' form_trans_self(d_norm, trans = function(x) {2*x})
 #'
 #' @name form_trans
 NULL
@@ -124,7 +177,7 @@ trans_bruteforce <- function(f_list, trans, ..., args_new) {
     if (is_single_number(f)) {
       new_p(f, "fin")
     } else {
-      form_retype(f, type = "fin")
+      form_retype(f, type = "fin", method = "piecelin")
     }
   })
   x_tbl_list <- lapply(f_list, meta_x_tbl)
@@ -157,7 +210,7 @@ trans_bruteforce <- function(f_list, trans, ..., args_new) {
   fin_res <- do.call(new_pdqr, args = call_args)
 
   # Retype back to input type
-  form_retype(fin_res, res_meta[["type"]])
+  form_retype(fin_res, res_meta[["type"]], method = "piecelin")
 }
 
 list_grid <- function(vec_list) {
