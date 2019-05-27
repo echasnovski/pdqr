8506375e09f92361713f9916f8004e13cdabee01
echasnovski <evgeni.chasnovski@gmail.com>
2019-05-27 21:28:25 +0300

    Implement "totvar" methods for `summ_distance()`.

--- R/summ_distance.R
+++ R/summ_distance.R
@@ -3,11 +3,12 @@ summ_distance <- function(f, g, method = "KS") {
   assert_pdqr_fun(f)
   assert_pdqr_fun(g)
   assert_type(method, is_string)
-  assert_in_set(method, c("KS"))
+  assert_in_set(method, c("KS", "totvar"))
 
   switch(
     method,
-    KS = distance_ks(f, g)
+    KS = distance_ks(f, g),
+    totvar = distance_totvar(f, g)
   )
 }
 
@@ -63,4 +64,74 @@ distance_ks_two_infin <- function(p_f, p_g) {
   ks_sep <- separation_ks_two_infin(p_f, p_g)
 
   abs(p_f(ks_sep) - p_g(ks_sep))
+}
+
+
+# Method "totvar" ---------------------------------------------------------
+# Maximum absolute difference in probabilities across all possible sets. In
+# other words, there should be some subset of real line (or a set of those)
+# probabilities of (more formally, limit of) which under `f` and `g` differ the
+# most. This set (of finite values for "fin" and of intervals for "infin") can
+# be expressed as `A = {x | f(x) > g(x)}` (`f` and `g` are d-functions) or
+# `B = {x | f(x) < g(x)}`.
+# However, absolute differences in probabilities for `A` and `B` are equal. This
+# is because:
+# `0 = 1 - 1 = (P_f(A) + P_f(B) + P_f(C)) - (P_g(A) + P_g(B) + P_g(C))`, where
+# `P_f` and `P_g` are probability measures of `f` and `g`;
+# `C = {x | f(x) = g(x)}`.
+# By definitions: `abs(P_f(A) - P_g(A)) = P_f(A) - P_g(A)`;
+# `abs(P_f(B) - P_g(B)) = P_g(B) - P_f(B)`; `P_f(C) = P_g(C)`.
+# Therefore: `abs(P_f(A) - P_g(A)) = abs(P_f(B) - P_g(B))`.
+distance_totvar <- function(f, g) {
+  d_f <- as_d(f)
+  d_g <- as_d(g)
+
+  num_fin <- (meta_type(f) == "fin") + (meta_type(g) == "fin")
+
+  switch(
+    as.character(num_fin),
+    `0` = distance_totvar_two_infin(d_f, d_g),
+    # A target set is all `x` values of "fin" pdqr-function. Its probability
+    # under "fin" is 1 and under "infin" is zero because it is countable.
+    `1` = 1,
+    `2` = distance_totvar_two_fin(d_f, d_g)
+  )
+}
+
+distance_totvar_two_infin <- function(d_f, d_g) {
+  # `{x | d_f(x) > d_g(x)}` is a union of intervals where `d_f(x) - d_g(x)` has
+  # constant positive sign. `d_f(x) - d_g(x)` can change sign in two cases:
+  # - When `d_f` and `d_g` intersect.
+  # - When either `d_f` or `d_g` shows discontinuity on edges.
+  x_inters <- compute_density_crossings(d_f, d_g)
+
+  # This might introduce duplicate elements on the edges (if `d_f` and `d_g`
+  # intersect on any support edge) but they will introduce "interval" with zero
+  # "sign" which will later be ignored.
+  x_lim <- sort(c(x_inters, meta_support(d_f), meta_support(d_g)))
+
+  interval_center <- (x_lim[-1] + x_lim[-length(x_lim)]) / 2
+  pos_sign_inds <- which(d_f(interval_center) > d_g(interval_center))
+
+  # Note: if `pos_sign_inds` is empty, then `f` and `g` are identical. In that
+  # case both `x_lim_left` and `x_lim_right` are empty and `sum()` later will
+  # return 0, which is correct answer.
+  x_lim_left <- x_lim[pos_sign_inds]
+  x_lim_right <- x_lim[pos_sign_inds+1]
+
+  p_f <- as_p(d_f)
+  p_g <- as_p(d_g)
+
+  # Output distance is total difference in probabilities of intervals where `f`
+  # is greater than `g`.
+  sum(
+    (p_f(x_lim_right) - p_f(x_lim_left)) - (p_g(x_lim_right) - p_g(x_lim_left))
+  )
+}
+
+distance_totvar_two_fin <- function(d_f, d_g) {
+  union_x <- union_x(d_f, d_g)
+  prob_diff <- d_f(union_x) - d_g(union_x)
+
+  sum(prob_diff[prob_diff > 0])
 }
--- tests/testthat/test-summ_distance.R
+++ tests/testthat/test-summ_distance.R
@@ -7,12 +7,22 @@ test_that("summ_distance works", {
   p_f <- as_p(punif)
   p_g <- as_p(punif, max = 0.5)
 
+  # Method "KS"
   expect_equal(summ_distance(p_f, p_g, method = "KS"), 0.5)
+
+  # Method "totvar"
+  expect_equal(summ_distance(p_f, p_g, method = "totvar"), 0.5)
+  expect_equal(
+    summ_distance(new_d(1:2, "fin"), new_d(1:2, "infin"), method = "totvar"),
+    1
+  )
 })
 
 test_that("summ_distance returns 0 for identical inputs", {
   expect_equal(summ_distance(d_fin, d_fin, method = "KS"), 0)
   expect_equal(summ_distance(d_infin, d_infin, method = "KS"), 0)
+  expect_equal(summ_distance(d_fin, d_fin, method = "totvar"), 0)
+  expect_equal(summ_distance(d_infin, d_infin, method = "totvar"), 0)
 })
 
 test_that("summ_distance validates input", {
@@ -139,7 +149,7 @@ test_that("distance_ks works with non-overlapping supports", {
 test_that("distance_ks works with dirac-like functions", {
   # K-S distance when "dirac" function is involved should be essentially (but
   # not exactly) the same as if it is replaced with corresponding "fin" (except
-  # the case when the other one is "fin" with one of points lying inside "dira"
+  # the case when the other one is "fin" with one of points lying inside "dirac"
   # support)
   d_dirac <- new_d(2, "infin")
   d_dirac_fin <- new_d(2, "fin")
@@ -171,3 +181,115 @@ test_that("distance_ks works with different pdqr classes", {
 
 # distance_ks_two_infin ---------------------------------------------------
 # Tested in `distance_ks()`
+
+
+# distance_totvar ---------------------------------------------------------
+test_that("distance_totvar works with two 'fin' functions", {
+  p_f <- new_p(1:3, "fin")
+  p_g <- new_p(1:3+1.5, "fin")
+  expect_equal(distance_totvar(p_f, p_g), 1)
+  # Checking twice to test independence of argument order
+  expect_equal(distance_totvar(p_g, p_f), 1)
+
+  p_f <- new_p(data.frame(x = 1:4, prob = 1:4/10), "fin")
+  p_g <- new_p(data.frame(x = 1:4, prob = 4:1/10), "fin")
+  expect_equal(distance_totvar(p_f, p_g), 0.4)
+
+  p_f <- new_p(1:10, "fin")
+  p_g <- new_p(5:14, "fin")
+  expect_equal(distance_totvar(p_f, p_g), 0.4)
+})
+
+test_that("distance_totvar works with mixed-type functions", {
+  expect_equal(distance_totvar(d_fin, d_infin), 1)
+  # Checking twice to test independence of argument order
+  expect_equal(distance_totvar(d_infin, d_fin), 1)
+})
+
+test_that("distance_totvar works with two 'infin' functions", {
+  # Multiple density intersections in real-world example
+  p_f <- new_p(data.frame(x = c(1:3, 5, 7), y = c(0, 0.5, 0, 0.25, 0)), "infin")
+  p_g <- new_p(
+    data.frame(x = c(1:3, 5, 7) + 0.5, y = c(0, 0.5, 0, 0.25, 0)), "infin"
+  )
+  # `{x | d_f(x) > d_g(x)}` is a union of (1; 2.25) and (3.4; 5.25)
+  out_ref <- ((p_f(2.25)-p_f(1)) + (p_f(5.25)-p_f(3.4))) -
+    ((p_g(2.25)-p_g(1)) + (p_g(5.25)-p_g(3.4)))
+
+  expect_equal(distance_totvar(p_f, p_g), out_ref)
+  # Checking twice to test independence of argument order
+  expect_equal(distance_totvar(p_g, p_f), out_ref)
+
+  # Non-intersecting densities in real-world example
+  p_f <- as_p(punif)
+  p_g <- as_p(punif, min = -1, max = 3)
+  expect_equal(distance_totvar(p_f, p_g), 0.75)
+
+  # Common y-zero plateau #1
+  p_f <- new_p(data.frame(x = 1:4, y = c(1, 0, 0, 1)), "infin")
+  p_g <- new_p(data.frame(x = 0:5, y = c(0, 1, 0, 0, 1, 0)/2), "infin")
+  # `{x | d_f(x) > d_g(x)}` is a union of (1; 2) and (3; 4)
+  out_ref <- ((p_f(2)-p_f(1)) + (p_f(4)-p_f(3))) -
+    ((p_g(2)-p_g(1)) + (p_g(4)-p_g(3)))
+  expect_equal(distance_totvar(p_f, p_g), out_ref)
+
+  # Common y-zero plateau #2
+  p_f <- new_p(data.frame(x = 1:4, y = c(0, 1, 0, 0)), "infin")
+  p_g <- new_p(data.frame(x = 3:6, y = c(0, 0, 1, 0)), "infin")
+  expect_equal(distance_totvar(p_f, p_g), 1)
+})
+
+test_that("distance_totvar returns zero with identical inputs", {
+  expect_equal(distance_totvar(d_fin, d_fin), 0)
+  expect_equal(distance_totvar(d_infin, d_infin), 0)
+
+  d_dirac <- new_d(2, "infin")
+  expect_equal(distance_totvar(d_dirac, d_dirac), 0)
+})
+
+test_that("distance_totvar works with non-overlapping supports", {
+  cur_fin_1 <- new_p(1:4, "fin")
+  cur_fin_2 <- new_p(5:6, "fin")
+  cur_infin_1 <- new_p(data.frame(x = 1:4, y = c(1, 1)/3), "infin")
+  cur_infin_2 <- new_p(data.frame(x = 5:6, y = c(1, 1)), "infin")
+  cur_infin_3 <- new_p(data.frame(x = 4:5, y = c(1, 1)), "infin")
+
+  # "Two fin"
+  expect_equal(distance_totvar(cur_fin_1, cur_fin_2), 1)
+  expect_equal(distance_totvar(cur_fin_2, cur_fin_1), 1)
+
+  # "Two infin"
+  expect_equal(distance_totvar(cur_infin_1, cur_infin_2), 1)
+  expect_equal(distance_totvar(cur_infin_2, cur_infin_1), 1)
+  # "Touching" supports
+  expect_equal(distance_totvar(cur_infin_1, cur_infin_3), 1)
+  expect_equal(distance_totvar(cur_infin_3, cur_infin_1), 1)
+})
+
+test_that("distance_totvar works with dirac-like functions", {
+  # Total variation distance when "dirac" function is involved should be
+  # essentially (but not exactly) the same as if it is replaced with
+  # corresponding "fin" (except the case when the other one is "fin" with one of
+  # points lying inside "dirac" support)
+  d_dirac <- new_d(2, "infin")
+  d_dirac_fin <- new_d(2, "fin")
+
+  # "Two infin" case
+  expect_equal(
+    distance_totvar(d_infin, d_dirac), distance_totvar(d_infin, d_dirac_fin)
+  )
+  # Non-overlapping dirac-like functions
+  expect_equal(distance_totvar(d_dirac, new_d(3, "infin")), 1)
+})
+
+test_that("distance_totvar works with different pdqr classes", {
+  expect_equal(distance_totvar(d_fin, d_infin), distance_totvar(p_fin, q_infin))
+})
+
+
+# distance_totvar_two_infin -----------------------------------------------
+# Tested in `distance_totvar()`
+
+
+# distance_totvar_two_fin -------------------------------------------------
+# Tested in `distance_totvar()`
