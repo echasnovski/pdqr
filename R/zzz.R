50ec5b2d0620b52b45af39e1a19c895ecf0909fd
echasnovski <evgeni.chasnovski@gmail.com>
2019-06-07 21:50:03 +0300

    Implement method for `pillar::type_sum()` for pretty printing of pdqr-functions in list-columns of tibble.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
+#nocov start
 .onLoad <- function(libname, pkgname) {
-  #nocov start
+  # Set default options
   default_options <- list(
     pdqr.group_gen.n_sample = 10000,
     pdqr.group_gen.args_new = list(),
@@ -11,6 +12,38 @@
     options(default_options[toset])
   }
 
+  # Register external methods
+  if (requireNamespace("pillar", quietly = TRUE)) {
+    register_s3_method("pillar", "type_sum", "pdqr", fun = type_sum.pdqr)
+  }
+
   invisible()
-  #nocov end
 }
+
+# Pretty printing of pdqr-functions as elements of list-columns in tibble
+# (tidyverse package).
+# Only one method for "pdqr" class is implemented (instead of four more quick
+# methods for every class separately) because of possible collision with classes
+# "p", "d", "q", "r" from some other package. This implmementation has not very
+# big overhead (around 4 microseconds).
+type_sum.pdqr <- function(x) {
+  paste0(meta_class(x), "-fun")
+}
+
+# Adapted from github.com/tidyverse/googledrive ('dplyr-compat.R')
+register_s3_method <- function(pkg, generic, class, fun) {
+  envir <- asNamespace(pkg)
+
+  if (pkg %in% loadedNamespaces()) {
+    registerS3method(generic, class, fun, envir = envir)
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) {
+      registerS3method(generic, class, fun, envir = envir)
+    }
+  )
+}
+#nocov end
