9b4f350ec5372a25684af560cbeb90854267713b
echasnovski <evgeni.chasnovski@gmail.com>
2019-07-02 20:29:27 +0300

    Create 'pdqr-package' help page.

--- R/pdqr-package.R
+++ R/pdqr-package.R
@@ -1,3 +1,29 @@
+# Package description -----------------------------------------------------
+#' pdqr: Work with Custom Distribution Functions
+#'
+#' @details Excerpt of important documentation:
+#' - README and vignettes provide overview of package functionality.
+#' - Documentation of [meta_*()][meta_all()] functions describes implementation
+#' details of pdqr-functions.
+#'     - Documentation of [print()][methods-print] and [plot()][methods-plot]
+#' methods describes how you can interactively explore properties of
+#' pdqr-functions.
+#' - Documentation of [new_*()][new_p()] functions describes the process of
+#' creating pdqr-functions.
+#' - Documentation of [as_*()][as_p()] functions describes the process of
+#' updating class of pdqr-functions.
+#' - Documentation of `form_*()` functions describes how different
+#' transformation functions work. Important pages are for [form_trans()] and
+#' [Pdqr methods for S3 group generic functions][methods-group-generic].
+#' - Documentation of `summ_*()` functions describes how different summary
+#' functions work. A good place to start is [summ_center()].
+#' - Documentation of [`region_*()`][region] functions describes functionality
+#' to work with regions: data frames defining subset of one dimensional real
+#' line.
+#'
+"_PACKAGE"
+
+
 # Methods for pretty printing ---------------------------------------------
 # Methods for pretty printing of pdqr-functions as string. Usually used for
 # printing them as elements of list-columns in data frame or tibble (tidyverse
